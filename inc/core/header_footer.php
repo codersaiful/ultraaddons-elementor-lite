305106ca95f696e7c247dc1cba502cb96b497a34
Saiful Islam <codersaiful@gmail.com>
2021-02-18 10:17:29 +0600

    still working for header footer

--- inc/core/header_footer.php
+++ inc/core/header_footer.php
@@ -10,10 +10,12 @@ defined( 'ABSPATH' ) || die();
 class Header_Footer {
     
     public static function init() {
-        add_action( 'get_header', [__CLASS__, 'show_header'], 10, 2 );
+        //add_action( 'get_header', [__CLASS__, 'show_header'], 10, 2 );
+        add_action( 'wp_body_open', [__CLASS__, 'show_header']);
     }
     
-    public static function show_header( $name, $args ) {
+    public static function show_header() {
+
         (int) $select_post_id = 2304;
         if ( \Elementor\Plugin::instance()->db->is_built_with_elementor( $select_post_id ) ) {
             echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display( $select_post_id );
