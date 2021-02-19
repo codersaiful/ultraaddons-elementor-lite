ede5a53fb500bc98b8ee6594b97b238e1d45c57a
Saiful Islam <codersaiful@gmail.com>
2021-02-19 06:31:22 +0600

    working for header footer

--- admin/pages/header-footer.php
+++ admin/pages/header-footer.php
@@ -1,7 +1,26 @@
 <?php
 
+use UltraAddons\Core\Template_List;
+
 defined( 'ABSPATH' ) || die();
 
 
+$template_obj = new Template_List();
+$templates = $template_obj->get_templates();
+
+var_dump($templates);
+if( is_array( $templates ) && count( $templates ) > 0 ){
 ?>
-<h1>Header Footer</h1>
+<select class="ultraddons-select" name="ultraddons-header-id">
+    <?php
+    foreach( $templates as $templ_id => $templ_name ){
+        echo "<option value='{$templ_id}'>{$templ_name}</option>";
+    }
+    ?>
+</select>
+<?php
+}else{
+?>
+    <h2 class="ultraaddons-header-footer-not"><?php echo esc_html__( "There is no Template founded in Elementor Library", 'ultraaddons' ); ?></h2>
+<?php   
+}
---
+++ inc/classes/template-list.php
@@ -0,0 +1,39 @@
+<?php
+namespace UltraAddons\Core;
+
+/**
+ * Getting Elementor Template List
+ * 
+ * @since 1.0.1.0
+ */
+class Template_List {
+    public $templates = [];
+    
+    public function __construct() {
+        
+    }
+    
+    public function get_templates() {
+
+        $args = array(
+            'post_type'     =>  'elementor_library',
+            'post_status'   =>  'publish'
+        );
+        $query = get_posts( $args );
+
+        // Check, If post not found , Direct return main choicese
+        if( empty( $query ) ){
+            $this->templates = [];
+        }
+
+        //If found post, then itarate
+        if( is_array( $query ) && count( $query ) > 0 ){
+            foreach( $query as $q_post ){
+                $id = (int) $q_post->ID;
+                $this->templates[$id] = $q_post->post_title;
+            }
+        }
+        
+        return $this->templates;
+    }
+}
