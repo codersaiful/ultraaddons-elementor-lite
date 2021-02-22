2f460d6586b6f1bd0d435008713d37b0fcd58689
Saiful Islam <codersaiful@gmail.com>
2021-02-22 11:51:12 +0600

    working for widgets list

--- admin/pages/widgets.php
+++ admin/pages/widgets.php
@@ -6,6 +6,30 @@ defined( 'ABSPATH' ) || die();
 
 $widgets = Widgets_Manager::widgets();
 
-var_dump( $widgets );
+
 ?>
-<h1>Widgets</h1>
+
+<div class="ultraaddons-section ua-widgets-wrapper">
+    <div class="ua-section-inside">
+        <div class="ua-header">
+            <h1><?php echo esc_html__( 'Widgets', 'ultraaddons' ); ?></h1>
+        </div>
+        
+        <div class="ua-sectioon-content">
+            <div class="ua-content-inside">
+                
+                <form class="ua-widget-list-form" action="" method="post">
+                    <?php 
+                    foreach( $widgets as $widget ){
+                        var_dump($widget);
+                    ?>
+                    <div class="ua-widget-item-wrappper">
+                        
+                    </div>
+                    <?php } ?>
+                </form>
+                
+            </div>
+        </div>
+    </div>
+</div>
