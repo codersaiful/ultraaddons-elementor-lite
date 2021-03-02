5b21b347aa6f9812c8f9124f8d2b47b4f71bdb43
Saiful Islam <codersaiful@gmail.com>
2021-03-02 17:41:08 +0600

    widget showing in basic or general category feature working

---
+++ admin/pages/settings.php
@@ -0,0 +1,68 @@
+<?php
+
+use UltraAddons\Core\Settings;
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Controlling Form Submission
+ */
+$form_datas = filter_input_array(INPUT_POST);
+
+do_action( 'ultraaddons_save_data', $form_datas, 'settings' );
+
+$key = Settings::$key;
+
+if( $form_datas && $key ){
+    update_option( $key, $form_datas );
+}
+$current_data = Settings::get_data();
+
+
+$category_slug = Settings::get_widget_category();
+?>
+
+<div class="ultraaddons-section ua-option-wrapper">
+    <div class="ua-section-inside">
+        <div class="ua-header">
+            <h1 class="ua-page-title"><?php echo esc_html__( 'Settings', 'ultraaddons' ); ?></h1>
+        </div>
+        
+        <div class="ua-sectioon-content">
+            <div class="ua-content-inside">
+
+                <form class="ua-header-footer-form" action="" method="post">
+                    <div class="ua-form-wrappper">
+                    
+                        
+                    
+
+
+                    <div class="ultraaddons-field-container field-container-category">
+                        <label class="field-label field-label-header-choose"><?php echo esc_html__( 'Widget Showing in', 'ultraaddons' ); ?></label>    
+                        <select class="ultraddons-select" name="widget_in">
+                            <option value="" ><?php echo esc_html__( 'Only UltraAddons', 'ultraaddons' ); ?></option>
+                            <option value="basic" <?php echo $category_slug == 'basic' ? 'selected' : ''; ?>><?php echo esc_html__( 'Basic', 'ultraaddons' ); ?></option>
+                            <option value="general" <?php echo $category_slug == 'general' ? 'selected' : ''; ?>><?php echo esc_html__( 'General', 'ultraaddons' ); ?></option>
+                            
+                        </select>
+                        <div class="ua-form-message">
+                            <p>
+                                Widget shows in <b>Addons - UltraAddons</b> Category of Elementor(in Elementor Edit Screen). If you want to show UltraAddons Widget
+                                    in Basic or General category, Choose and Save Change.
+                            </p>
+                        </div>
+                    </div>
+
+                     
+
+                    </div> <!-- /.ua-form-wrappper -->
+                    <div class="ua-widget-footer">
+                        <button class="primary button button-primary ua-primary ua-no-update" type="submit"><?php echo esc_html__( 'Save Change', 'ultraaddons' ); ?></button>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
+    </div>
+</div>
