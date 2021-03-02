d7bb1132a0843da36d2d7c3f3536494161602a40
Saiful Islam <codersaiful@gmail.com>
2021-03-02 14:19:09 +0600

    ua-ultraaddons-[widget] class has removed

--- inc/base/base.php
+++ inc/base/base.php
@@ -187,9 +187,9 @@ class Base extends Widget_Base{
     public function get_html_wrapper_class() {
         $wr_class = strtolower( str_replace( '_', '-', $this->get_pure_name() ) );
         $html_class = parent::get_html_wrapper_class();
-        $html_class .= ' ultraaddons-element';
+        $html_class .= ' ua-element';
         $html_class .= ' ua-element-' . $wr_class . ' ';
-        $html_class .= ' ua-' . $this->get_name() . ' ';
+        //$html_class .= ' ua-' . $this->get_name() . ' ';
         
         return rtrim( $html_class );
     }
