eb320039a8bdb7a84086bc6d752f870b3f97bba4
Saiful Islam <codersaiful@gmail.com>
2021-03-07 17:38:46 +0600

    a new trait is adding name background

---
+++ inc/traits/background.php
@@ -0,0 +1,15 @@
+<?php
+namespace UltraAddons\Traits;
+
+use Elementor\Controls_Manager;
+
+use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
+use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
+use Elementor\Group_Control_Typography;
+use Elementor\Icons_Manager;
+use Elementor\Group_Control_Border;
+use Elementor\Utils;
+
+trait Background{
+    
+}
