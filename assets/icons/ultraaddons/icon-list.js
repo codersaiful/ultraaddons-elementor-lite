a3abed0be2125bff027ccbfd2631cd97f7dfbed4
Saiful Islam <codersaiful@gmail.com>
2021-03-07 16:05:07 +0600

    icon update on list

--- assets/icons/ultraaddons/icon-list.js
+++ assets/icons/ultraaddons/icon-list.js
@@ -1,12 +1,30 @@
 {
   "icons": [
-	"ultraaddons",
-	"countdown",
-	"icon-box",
-	"price-table",
-	"progress-bar",
-	"review",
-	"slider",
-	"team-member"
+	"ultraaddons"
+        "animated-header"
+        "blog-list"
+        "button"
+        "cart"
+        "clock"
+        "contact-form-7-light"
+        "contact-form-7"
+        "contact-form"
+        "counter"
+        "crown"
+        "header"
+        "hover"
+        "icon-box"
+        "list"
+        "offer-card"
+        "pricing-table"
+        "product-slider"
+        "product-table"
+        "product"
+        "rating"
+        "skillbar"
+        "slider"
+        "team-box"
+        "team-member"
+        "testimonial"
 	]
 }
