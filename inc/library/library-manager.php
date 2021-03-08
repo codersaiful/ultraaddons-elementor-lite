013aaf5727ee2dcd3f040215e9c773a80a77289d
Saiful Islam <codersaiful@gmail.com>
2021-03-08 20:35:28 +0600

    working for libarary

---
+++ inc/library/library-manager.php
@@ -0,0 +1,36 @@
+<?php
+namespace UltraAddons\Library;
+
+defined('ABSPATH') || die();
+
+class Library_Manager{
+    public static function init(){
+        var_dump(__DIR__ . '/templates/panel.php');
+        add_action( 'elementor/editor/footer', [ __CLASS__, 'render_panel_html' ] );
+    }
+    
+    public static function render_panel_html(){
+        include __DIR__ . '/templates/panel.php';
+        var_dump("Library");
+        ?>
+            
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+<h1>Hello World Bangladesh.</h1>    
+        <?php
+    }
+}
