f31a52c494e0da93e26bcdef64ad977d8cbe09ef
Saiful Islam <codersaiful@gmail.com>
2021-03-10 21:58:44 +0600

    working on Elementor Demo Arrange

--- inc/library/library-manager.php
+++ inc/library/library-manager.php
@@ -7,7 +7,7 @@ class Library_Manager{
     
     public static function init(){
         add_action( 'elementor/init', [__CLASS__, 'testing'] );
-        
+
 //        add_action( 'elementor/editor/footer', [__CLASS__, 'render_panel_html'] );
     }
     
@@ -18,13 +18,9 @@ class Library_Manager{
     
     public static function testing() {
                 include __DIR__ . '/library-source.php';
-
-                // Unregister source with closure binding, thank Steve.
-                $unregister_source = function($id) {
-                        unset( $this->_registered_sources[ $id ] );
-                };
-                $unregister_source->call( \Elementor\Plugin::instance()->templates_manager, 'remote');
-                //var_dump( \Elementor\Plugin::instance()->templates_manager );
+                
+                \Elementor\Plugin::instance()->templates_manager->unregister_source( 'remote' );
                 \Elementor\Plugin::instance()->templates_manager->register_source( 'Library_Source' );
+                
         }
 }
