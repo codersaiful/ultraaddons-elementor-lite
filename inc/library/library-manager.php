2beea9c9208fb96a11d362d1e9124fd5f2d8a8b8
Saiful Islam <codersaiful@gmail.com>
2021-03-10 18:02:43 +0600

    working on library

--- inc/library/library-manager.php
+++ inc/library/library-manager.php
@@ -6,24 +6,25 @@ defined('ABSPATH') || die();
 class Library_Manager{
     
     public static function init(){
-        add_action( 'elementor/init', [__CLASS__, 'render_panel_html'] );
+        add_action( 'elementor/init', [__CLASS__, 'testing'] );
+        
+//        add_action( 'elementor/editor/footer', [__CLASS__, 'render_panel_html'] );
     }
     
     public static function render_panel_html(){
-        var_dump(__DIR__ . '/templates/panel.php');
+        //var_dump(__DIR__ . '/templates/panel.php');
         include __DIR__ . '/templates/test.php';
     }
     
     public static function testing() {
-                var_dump(1232);
-                include 'includes/source.php';
+                include __DIR__ . '/library-source.php';
 
                 // Unregister source with closure binding, thank Steve.
                 $unregister_source = function($id) {
                         unset( $this->_registered_sources[ $id ] );
                 };
                 $unregister_source->call( \Elementor\Plugin::instance()->templates_manager, 'remote');
-                var_dump($unregister_source);
-                //\Elementor\Plugin::instance()->templates_manager->register_source( 'Elementor\TemplateLibrary\Source_Custom' );
+                var_dump( \Elementor\Plugin::instance()->templates_manager );
+                \Elementor\Plugin::instance()->templates_manager->register_source( 'Library_Source' );
         }
 }
