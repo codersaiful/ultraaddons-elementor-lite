ad458ddfb7541db28f09eaeaeef2ea043d36f381
Saiful Islam <codersaiful@gmail.com>
2021-03-21 18:15:38 +0600

    Developer version updated

--- init.php
+++ init.php
@@ -43,7 +43,7 @@
 
 defined( 'ABSPATH' ) || die();
 
-define( 'ULTRA_ADDONS_VERSION', '1.0.4.0' );
+define( 'ULTRA_ADDONS_VERSION', '1.0.4.1' );
 define( 'ULTRA_ADDONS__FILE__', __FILE__ );
 define( 'ULTRA_ADDONS_BASE_NAME', plugin_basename( __FILE__ ) );
 define( 'ULTRA_ADDONS_DIR', plugin_dir_path( ULTRA_ADDONS__FILE__ ) );
