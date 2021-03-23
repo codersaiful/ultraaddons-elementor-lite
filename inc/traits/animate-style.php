8eb30cb7bcfc72b1c8871a4551ad5085f40ae9f5
Saiful Islam <codersaiful@gmail.com>
2021-03-23 11:30:25 +0600

    making animae style traits

--- inc/traits/animate-style.php
+++ inc/traits/animate-style.php
@@ -14,40 +14,5 @@ use Elementor\Utils;
 trait Animate_Style{
     
     
-    /**
-     * To add Gradient Background Control
-     * We will use this Trait
-     * and will use method gradient_background()
-     * 
-     * @param type $selectors
-     * @param string $name
-     */
-    public function gradient_background( $selectors = '', $name = ''){
-        if( ! is_string( $selectors ) || ! is_string( $name ) ){
-            return;
-        }
-        //Confirm String name
-        if( ! empty( $name ) ){
-            $name = 'bacground_' . $this->get_id();
-        }
-        $this->add_group_control(
-                Group_Control_Background::get_type(),
-                [
-                        'name' => 'btn_background',
-                        'types' => [ 'classic', 'gradient' ],
-                        'selector' => '{{WRAPPER}} ' . $selectors,
-                        'fields_options' => [
-                                'background' => [
-                                        'frontend_available' => true,
-                                ],
-                                'color' => [
-                                        'dynamic' => [],
-                                ],
-                                'color_b' => [
-                                        'dynamic' => [],
-                                ],
-                        ],
-                ]
-        );
-    }
+    
 }
