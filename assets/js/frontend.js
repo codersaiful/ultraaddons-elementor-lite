a4006f7b884bb7e1b0d230be278f8cdca7188691
Saiful Islam <codersaiful@gmail.com>
2021-04-04 10:34:31 +0600

    magnify popul code added at frontend.js we have to work later

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -283,4 +283,44 @@
                 }
             });
     }
+    
+    
+    /*     Magnific Popup js
+     -------------------------------------*/
+    $('.video_btn').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        preloader: true,
+    });
+
+    //Magnific popup video
+     $('.play-btn').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>',
+
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                },
+                vimeo: {
+                    index: 'vimeo.com/',
+                    id: '/',
+                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                },
+                gmaps: {
+                    index: 'https://maps.google.',
+                    src: '%id%&output=embed'
+                }
+            },
+            srcAction: 'iframe_src',
+        }
+        // other options
+    });
+        
 } (jQuery, window));
