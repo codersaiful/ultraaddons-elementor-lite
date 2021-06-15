9d56ea0acf511be06b43c197707a90b908989df7
Saiful Islam <codersaiful@gmail.com>
2021-06-15 18:03:03 +0600

    WC Products Widget Added

--- inc/core/widgets_array.php
+++ inc/core/widgets_array.php
@@ -151,6 +151,17 @@ $widgetsArray = [
                 __( 'Basic', 'ultraaddons' ),
             ],
     ],
+    
+    'WC_Products' => [
+            'name'  => __( 'WooCommerce Products', 'ultraaddons' ),
+            'is_free'   => true,
+            'icon'      => 'uicon-product',
+            'cat'       => [
+                __( 'Basic', 'ultraaddons' ),
+            ],
+    ],
+    
+    
     'WC_Categories' => [
             'name'  => __( 'Product Categories', 'ultraaddons' ),
             'is_free'   => true,
