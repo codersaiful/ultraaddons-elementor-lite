2836898b3b4197f97e179c5a717af5d4c3655a43
Saiful Islam <codersaiful@gmail.com>
2021-06-26 12:56:41 +0600

    uicon-ultraaddons

--- inc/library/templates.php
+++ inc/library/templates.php
@@ -8,7 +8,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <script type="text/template" id="tmpl-haTemplateLibrary__header-logo">
     <span class="haTemplateLibrary__logo-wrap">
-		<i class="ua ua-ultraaddons"></i>
+		<i class="uicon-ultraaddons"></i>
 	</span>
     <span class="haTemplateLibrary__logo-title">{{{ title }}}</span>
 </script>
