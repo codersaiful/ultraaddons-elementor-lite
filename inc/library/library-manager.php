c1547b7d73a2dec009c5ead5c1b0b4ab3349a456
Saiful Islam <codersaiful@gmail.com>
2021-06-26 11:10:41 +0600

    working basedonHap

--- inc/library/library-manager.php
+++ inc/library/library-manager.php
@@ -1,224 +1,132 @@
 <?php
-namespace UltraAddons\Library;
+namespace Happy_Addons\Elementor;
 
-use Elementor\Plugin;
+use Elementor\Core\Common\Modules\Ajax\Module as Ajax;
 
 defined('ABSPATH') || die();
 
-class Library_Manager{
-    private static $library_assets = ULTRA_ADDONS_URL . 'inc/library/assets/';
+class Library_Manager {
 
-    /**
-     * Initializing Library Manager
-     * Steps:
-     * register sourche
-     * 
-     * 
-     * **********************
-     * Actually we added it at the begining of plugin
-     * but working after 1.0.7.1
-     * 
-     * @package UltraAddons
-     */
-    public static function init(){
-        //ultraaddons_elementor();
-        #add_action( 'elementor/init', [__CLASS__, 'register_source'], 15 );
-        #add_action( 'elementor/editor/footer', [__CLASS__, 'render_panel_html'] );
-        
-        //var_dump(self::$library_assets);
-        /** 
-         * Enqueue
-         */
-        
-        // enqueue editor js for elementor.
-        add_action( 'elementor/editor/before_enqueue_scripts', [ __CLASS__, 'editor_scripts' ], 1);
+	protected static $source = null;
 
-        // print views and tab variables on footer.
-        add_action( 'elementor/editor/footer', [ __CLASS__, 'admin_inline_js' ] );
-        add_action( 'elementor/editor/footer', [ __CLASS__, 'print_views' ] );
-        
-        // enqueue editor css.
-        add_action( 'elementor/editor/after_enqueue_styles', [ __CLASS__, 'editor_styles' ] );
+	public static function init() {
+		add_action( 'elementor/editor/footer', [ __CLASS__, 'print_template_views' ] );
+		add_action( 'elementor/ajax/register_actions', [ __CLASS__, 'register_ajax_actions' ] );
+	}
 
-        // enqueue modal's preview css.
-        add_action( 'elementor/preview/enqueue_styles', [ __CLASS__, 'preview_styles' ] );
+	public static function print_template_views() {
+		include_once HAPPY_ADDONS_DIR_PATH . 'templates/template-library/templates.php';
+	}
 
-    }
-    
-    public static function editor_scripts(){
-        wp_enqueue_script( 
-			'ultraaddons-library-editor-script', 
-			self::$library_assets . 'js/editor.js', 
-			array('jquery', 'underscore', 'backbone-marionette'), 
-			ULTRA_ADDONS_VERSION,
+	public static function enqueue_assets() {
+		wp_enqueue_style(
+			'happy-addons-templates-library',
+			HAPPY_ADDONS_ASSETS . 'admin/css/template-library.min.css',
+			[
+				'elementor-editor',
+			],
+			HAPPY_ADDONS_VERSION
+		);
+
+		wp_enqueue_script(
+			'happy-addons-templates-library',
+			HAPPY_ADDONS_ASSETS . 'admin/js/template-library.min.js',
+			[
+				'happy-elementor-addons-editor',
+				'jquery-hover-intent',
+			],
+			HAPPY_ADDONS_VERSION,
 			true
 		);
-    }
-    public static function admin_inline_js(){
-        ?>
-		<script type="text/javascript" >
+	}
 
-		var ElementsKitLibreryData = {
-			"libraryButton": "Elements Button",
-			"modalRegions": {
-				"modalHeader": ".dialog-header",
-				"modalContent": ".dialog-message"
-			},
-			"license": {
-				"activated": true,
-				"link": ""
-			},
-			"tabs": {
-				"elementskit_page": {
-					"title": "Ready Pages",
-					"data": [],
-					"sources": ["elementskit-theme", "elementskit-api"],
-					"settings": {
-						"show_title": true,
-						"show_keywords": true
-					}
-				},
-				"elementskit_header": {
-					"title": "Headers",
-					"data": [],
-					"sources": ["elementskit-theme", "elementskit-api"],
-					"settings": {
-						"show_title": false,
-						"show_keywords": true
-					}
-				},
-				"elementskit_footer": {
-					"title": "Footers",
-					"data": [],
-					"sources": ["elementskit-theme", "elementskit-api"],
-					"settings": {
-						"show_title": false,
-						"show_keywords": true
-					}
-				},
-				"elementskit_section": {
-					"title": "Sections",
-					"data": [],
-					"sources": ["elementskit-theme", "elementskit-api"],
-					"settings": {
-						"show_title": false,
-						"show_keywords": true
-					}
-				},
-				"elementskit_widget": {
-					"title": "Widget Presets",
-					"data": [],
-					"sources": ["elementskit-theme", "elementskit-api"],
-					"settings": {
-						"show_title": false,
-						"show_keywords": true
-					}
-				},
-				// "local": {
-				// 	"title": "My Library",
-				// 	"data": [],
-				// 	"sources": ["elementskit-local"],
-				// 	"settings": []
-				// }
-			},
-			"defaultTab": "elementskit_page"
-		};
+	/**
+	 * Undocumented function
+	 *
+	 * @return Library_Source
+	 */
+	public static function get_source() {
+		if ( is_null( self::$source ) ) {
+			self::$source = new Library_Source();
+		}
 
-		</script> <?php
-    }
-    public static function print_views(){
-        
-    }
-    public static function editor_styles(){
-        wp_enqueue_style( 'elementskit-library-editor-style', self::$library_assets . 'css/editor.css', array(), ULTRA_ADDONS_VERSION);
-    }
-    public static function preview_styles(){
-        wp_enqueue_style( 'elementskit-library-preview-style', self::$library_assets . 'css/preview.css', array(), ULTRA_ADDONS_VERSION );
-    }
+		return self::$source;
+	}
 
-    /**
-     * Register Source
-     * and adding our Template at existing Template
-     * 
-     * @since 1.0.7.1
-     * @date 24.6.2021 (d.m.y)
-     * @author Saiful Islam<codersaiful@gmail.com>
-     */
-    public static function register_source(){
-        // Unregister source with closure binding, thank Steve.
-       $unregister_source = function($id) {
-               unset( $this->_registered_sources[ $id ] );
-       };
-       
-//       $hell = Plugin::instance()->templates_manager->get_registered_sources();
-//       var_dump($hell['remote']);
-       
-       //Unregister Default/existing Template of remote
-       $unregister_source->call( Plugin::instance()->templates_manager, 'remote');
-       Plugin::instance()->templates_manager->register_source( 'UltraAddons\Library\Library_Source' );
-    }
-    
-    public static function render_panel_html(){
-        include_once __DIR__ . '/templates.php';
-    }
-    
+	public static function register_ajax_actions( Ajax $ajax ) {
+		$ajax->register_ajax_action( 'get_ha_library_data', function( $data ) {
+			if ( ! current_user_can( 'edit_posts' ) ) {
+				throw new \Exception( 'Access Denied' );
+			}
+
+			if ( ! empty( $data['editor_post_id'] ) ) {
+				$editor_post_id = absint( $data['editor_post_id'] );
+
+				if ( ! get_post( $editor_post_id ) ) {
+					throw new \Exception( __( 'Post not found.', 'happy-elementor-addons' ) );
+				}
+
+				ha_elementor()->db->switch_to_post( $editor_post_id );
+			}
+
+			$result = self::get_library_data( $data );
+
+			return $result;
+		} );
+
+		$ajax->register_ajax_action( 'get_ha_template_data', function( $data ) {
+			if ( ! current_user_can( 'edit_posts' ) ) {
+				throw new \Exception( 'Access Denied' );
+			}
+
+			if ( ! empty( $data['editor_post_id'] ) ) {
+				$editor_post_id = absint( $data['editor_post_id'] );
+
+				if ( ! get_post( $editor_post_id ) ) {
+					throw new \Exception( __( 'Post not found', 'happy-elementor-addons' ) );
+				}
+
+				ha_elementor()->db->switch_to_post( $editor_post_id );
+			}
+
+			if ( empty( $data['template_id'] ) ) {
+				throw new \Exception( __( 'Template id missing', 'happy-elementor-addons' ) );
+			}
+
+			$result = self::get_template_data( $data );
+
+			return $result;
+		} );
+	}
+
+	public static function get_template_data( array $args ) {
+		$source = self::get_source();
+		$data = $source->get_data( $args );
+		return $data;
+	}
+
+	/**
+	 * Get library data from cache or remote
+	 *
+	 * type_tags has been added in version 2.15.0
+	 *
+	 * @param array $args
+	 *
+	 * @return array
+	 */
+	public static function get_library_data( array $args ) {
+		$source = self::get_source();
+
+		if ( ! empty( $args['sync'] ) ) {
+			Library_Source::get_library_data( true );
+		}
+
+		return [
+			'templates' => $source->get_items(),
+			'tags'      => $source->get_tags(),
+			'type_tags' => $source->get_type_tags(),
+		];
+	}
 }
-    
-    
 
-
-
-
-/**
- * All Backup Code for now, 
- * I will remove bottom all code
- * very asap
- * 
- */
-//$dddd = new UltraAddons\Library\Library_Source();
-//
-//add_action( 'elementor/init', function() {
-//       // Unregister source with closure binding, thank Steve.
-//       $unregister_source = function($id) {
-//               unset( $this->_registered_sources[ $id ] );
-//       };
-//
-//       $unregister_source->call( Plugin::instance()->templates_manager, 'remote');
-//       Plugin::instance()->templates_manager->register_source( 'UltraAddons\Library\Library_Source' );
-//}, 15 );
-
-class Library_Manager_Backup{
-    
-    public static function init(){
-        
-//        Plugin::instance()->templates_manager->register_source( '\UltraAddons\Library\Library_Source' );
-        
-        
-//        var_dump(get_option('ua_library_data'));
-//        update_option( 'ua_library_data', get_option('ha_library_cache') );
-        add_action( 'elementor/init', [__CLASS__, 'testing'] );
-
-//        add_action( 'elementor/editor/footer', [__CLASS__, 'render_panel_html'] );
-    }
-    
-    public static function render_panel_html(){
-        //var_dump(__DIR__ . '/templates/panel.php');
-        include __DIR__ . '/templates/test.php';
-    }
-    
-    public static function testing() {
-        Plugin::instance()->templates_manager->unregister_source('remote');
-//        var_dump(Plugin::instance()->templates_manager->get_registered_sources());
-//        var_dump(Library_Source::get_library_data());
-//        var_dump(Library_Source::$api_info_url);
-//        var_dump(Library_Source::get_library_data(true));
-
-                
-//                Plugin::instance()->templates_manager->unregister_source( 'remote' );
-                Plugin::instance()->templates_manager->register_source( '\UltraAddons\Library\Library_Source' );
-//                var_dump(Plugin::instance()->templates_manager->get_registered_sources());
-//                Plugin::instance()->templates_manager->register_source( '\UltraAddons\Library' );
-                //var_dump(Plugin::instance()->templates_manager->_registered_sources);//$_registered_sources
-                
-        }
-}
+Library_Manager::init();
--- inc/library/library-source.php
+++ inc/library/library-source.php
@@ -1,124 +1,61 @@
 <?php
-namespace UltraAddons\Library;
+/**
+ * Library api class
+ *
+ * @package HappyAddons
+ * @author HappyMonster
+ */
+namespace Happy_Addons\Elementor;
 
-use Elementor\TemplateLibrary\Source_Base as Source;
-use Elementor\Plugin;
+use Elementor\TemplateLibrary\Source_Base;
 
 defined( 'ABSPATH' ) || die();
 
-/**
- * Source Base Management here.
- * Taking help from: 
- * 
- * @link https://github.com/dinhtungdu/custom-elementor-source Getting help from Example Branch
- * @link https://dinhtungdu.github.io/create-your-own-elementor-template-library/ Tutorial
- * @link https://github.com/dinhtungdu/custom-elementor-library-dummy-api/ Demo Link
- * 
- * @Since 1.0.3.4
- * @by Saiful
- */
-class Library_Source extends Source {
+class Library_Source extends Source_Base {
 
 	/**
-	 * New library option key.
+	 * Template library data cache
 	 */
-	const LIBRARY_OPTION_KEY = 'ua_library_data';
+	const LIBRARY_CACHE_KEY = 'ha_library_cache';
 
 	/**
-	 * Timestamp cache key to trigger library sync.
+	 * Template info api url
+	 *
+	 * Updated api to v2 in version 2.15.0
 	 */
-	const TIMESTAMP_CACHE_KEY = 'ua_library_timestamp';
+	const API_TEMPLATES_INFO_URL = 'https://templates.happyaddons.com/wp-json/ha/v2/templates-info';
 
 	/**
-	 * API info URL.
-	 *
-	 * Holds the URL of the info API.
-	 *
-	 * @access public
-	 * @static
-         * 
-         * http://localhost/projects/custom-elementor-library/info.json
-	 * https://raw.githubusercontent.com/dinhtungdu/custom-elementor-library-dummy-api/master/info.json
-	 * @var string API info URL.
-         * 
-         * ## Comment From Prev ##
-         * public static $api_info_url = ULTRA_ADDONS_URL . 'inc/library/local-demo/info.json';
-         * 
-         * Final Link will be:
-         * https://library-api.ultraaddons.com/info.json
+	 * Template data api url
 	 */
-	public static $api_info_url = 'http://localhost/projects/custom-elementor-library/info.json';
+	const API_TEMPLATE_DATA_URL = 'https://templates.happyaddons.com/wp-json/ha/v1/templates/';
 
-	/**
-	 * API get template content URL.
-	 *
-	 * Holds the URL of the template content API.
-	 *
-	 * @access private
-	 * @static
-	 *
-         * http://localhost/projects/custom-elementor-library/templates/%d.json
-         * https://raw.githubusercontent.com/dinhtungdu/custom-elementor-library-dummy-api/master/templates/%d.json
-	 * @var string API get template content URL.
-         * 
-         * ## Comment from Prev ##
-         * private static $api_get_template_content_url = ULTRA_ADDONS_URL . 'inc/library/local-demo/templates/%d.json';
-         * //private static $api_get_template_content_url = 'https://raw.githubusercontent.com/dinhtungdu/custom-elementor-library-dummy-api/master/templates/%d.json';
-         * 
-         * #########Final Link:
-         * will be
-         * https://library-api.ultraaddons.com/templates/%d.json
-         * 
-	 */
-	private static $api_get_template_content_url = 'http://localhost/projects/custom-elementor-library/templates/%d.json';
-
-	/**
-	 * Get remote template ID.
-	 *
-	 * Retrieve the remote template ID.
-	 *
-	 * @access public
-	 *
-	 * @return string The remote template ID.
-	 */
 	public function get_id() {
-		return 'remote';
+		return 'happy-library';
 	}
 
-	/**
-	 * Get remote template title.
-	 *
-	 * Retrieve the remote template title.
-	 *
-	 * @access public
-	 *
-	 * @return string The remote template title.
-	 */
 	public function get_title() {
-		return __( 'Remote', 'custom-elementor-source' );
+		return __( 'Happy Library', 'happy-elementor-addons' );
 	}
 
-	/**
-	 * Register remote template data.
-	 *
-	 * Used to register custom template data like a post type, a taxonomy or any
-	 * other data.
-	 *
-	 * @access public
-	 */
 	public function register_data() {}
 
-	/**
-	 * Get remote templates.
-	 *
-	 * Retrieve remote templates from Elementor.com servers.
-	 *
-	 * @access public
-	 *
-	 * @param array $args Optional. Nou used in remote source.
-	 *
-	 * @return array Remote templates.
-	 */
+	public function save_item( $template_data ) {
+		return new \WP_Error( 'invalid_request', 'Cannot save template to a happpy library' );
+	}
+
+	public function update_item( $new_data ) {
+		return new \WP_Error( 'invalid_request', 'Cannot update template to a happpy library' );
+	}
+
+	public function delete_template( $template_id ) {
+		return new \WP_Error( 'invalid_request', 'Cannot delete template from a happpy library' );
+	}
+
+	public function export_template( $template_id ) {
+		return new \WP_Error( 'invalid_request', 'Cannot export template from a happpy library' );
+	}
+
 	public function get_items( $args = [] ) {
 		$library_data = self::get_library_data();
 
@@ -133,92 +70,93 @@ class Library_Source extends Source {
 		return $templates;
 	}
 
-	/**
-	 * Get templates data.
-	 *
-	 * Retrieve the templates data from a remote server.
-	 *
-	 * @access public
-	 * @static
-	 *
-	 * @param bool $force_update Optional. Whether to force the data update or
-	 *                                     not. Default is false.
-	 *
-	 * @return array The templates data.
-	 */
-	public static function get_library_data( $force_update = false ) {
-		self::get_info_data( $force_update );
+	public function get_tags() {
+		$library_data = self::get_library_data();
 
-		$library_data = get_option( self::LIBRARY_OPTION_KEY );
+		return ( ! empty( $library_data['tags'] ) ? $library_data['tags'] : [] );
+	}
 
-		if ( empty( $library_data ) ) {
-			return [];
-		}
+	public function get_type_tags() {
+		$library_data = self::get_library_data();
 
-		return $library_data;
+		return ( ! empty( $library_data['type_tags'] ) ? $library_data['type_tags'] : [] );
 	}
 
 	/**
-	 * Get info data.
+	 * Prepare template items to match model
 	 *
-	 * This function notifies the user of upgrade notices, new templates and contributors.
-	 *
-	 * @access private
-	 * @static
-	 *
-	 * @param bool $force_update Optional. Whether to force the data retrieval or
-	 *                                     not. Default is false.
-	 *
-	 * @return array|false Info data, or false.
+	 * @param array $template_data
+	 * @return array
 	 */
-	private static function get_info_data( $force_update = false ) {
+	private function prepare_template( array $template_data ) {
+		return [
+			'template_id' => $template_data['id'],
+			'title'       => $template_data['title'],
+			'type'        => $template_data['type'],
+			'thumbnail'   => $template_data['thumbnail'],
+			'date'        => $template_data['created_at'],
+			'tags'        => $template_data['tags'],
+			'isPro'       => $template_data['is_pro'],
+			'url'         => $template_data['url'],
+		];
+	}
 
-		$elementor_update_timestamp = get_option( '_transient_timeout_elementor_remote_info_api_data_' . ELEMENTOR_VERSION );
-		$update_timestamp = get_transient( self::TIMESTAMP_CACHE_KEY );
+	/**
+	 * Get library data from remote source and cache
+	 *
+	 * @param boolean $force_update
+	 * @return array
+	 */
+	private static function request_library_data( $force_update = false ) {
+		$data = get_option( self::LIBRARY_CACHE_KEY );
 
-		if ( $force_update || ! $update_timestamp || $update_timestamp != $elementor_update_timestamp ) {
+		if ( $force_update || false === $data ) {
 			$timeout = ( $force_update ) ? 25 : 8;
 
-			$response = wp_remote_get( self::$api_info_url, [
+			$response = wp_remote_get( self::API_TEMPLATES_INFO_URL, [
 				'timeout' => $timeout,
-				'body' => [
-					// Which API version is used.
-					'api_version' => ELEMENTOR_VERSION,
-					// Which language to return.
-					'site_lang' => get_bloginfo( 'language' ),
-				],
 			] );
 
 			if ( is_wp_error( $response ) || 200 !== (int) wp_remote_retrieve_response_code( $response ) ) {
-				set_transient( self::TIMESTAMP_CACHE_KEY, [], 2 * HOUR_IN_SECONDS );
-
+				update_option( self::LIBRARY_CACHE_KEY, [] );
 				return false;
 			}
 
-			$info_data = json_decode( wp_remote_retrieve_body( $response ), true );
+			$data = json_decode( wp_remote_retrieve_body( $response ), true );
 
-			if ( empty( $info_data ) || ! is_array( $info_data ) ) {
-				set_transient( self::TIMESTAMP_CACHE_KEY, [], 2 * HOUR_IN_SECONDS );
-
+			if ( empty( $data ) || ! is_array( $data ) ) {
+				update_option( self::LIBRARY_CACHE_KEY, [] );
 				return false;
 			}
 
-			if ( isset( $info_data['library'] ) ) {
-				update_option( self::LIBRARY_OPTION_KEY, $info_data['library'], 'no' );
-			}
-
-			set_transient( self::TIMESTAMP_CACHE_KEY, $elementor_update_timestamp, 12 * HOUR_IN_SECONDS );
+			update_option( self::LIBRARY_CACHE_KEY, $data, 'no' );
 		}
 
-		return $info_data;
+		return $data;
+	}
+
+	/**
+	 * Get library data
+	 *
+	 * @param boolean $force_update
+	 * @return array
+	 */
+	public static function get_library_data( $force_update = false ) {
+		self::request_library_data( $force_update );
+
+		$data = get_option( self::LIBRARY_CACHE_KEY );
+
+		if ( empty( $data ) ) {
+			return [];
+		}
+
+		return $data;
 	}
 
 	/**
 	 * Get remote template.
 	 *
 	 * Retrieve a single remote template from Elementor.com servers.
-	 *
-	 * @access public
 	 *
 	 * @param int $template_id The template ID.
 	 *
@@ -230,68 +168,30 @@ class Library_Source extends Source {
 		return $templates[ $template_id ];
 	}
 
-	/**
-	 * Save remote template.
-	 *
-	 * Remote template from Elementor.com servers cannot be saved on the
-	 * database as they are retrieved from remote servers.
-	 *
-	 * @access public
-	 *
-	 * @param array $template_data Remote template data.
-	 *
-	 * @return \WP_Error
-	 */
-	public function save_item( $template_data ) {
-		return new \WP_Error( 'invalid_request', 'Cannot save template to a remote source' );
-	}
+	public static function request_template_data( $template_id ) {
+		if ( empty( $template_id ) ) {
+			return;
+		}
 
-	/**
-	 * Update remote template.
-	 *
-	 * Remote template from Elementor.com servers cannot be updated on the
-	 * database as they are retrieved from remote servers.
-	 *
-	 * @access public
-	 *
-	 * @param array $new_data New template data.
-	 *
-	 * @return \WP_Error
-	 */
-	public function update_item( $new_data ) {
-		return new \WP_Error( 'invalid_request', 'Cannot update template to a remote source' );
-	}
+		$body = [
+			'home_url' => trailingslashit( home_url() ),
+			'version' => HAPPY_ADDONS_VERSION,
+		];
 
-	/**
-	 * Delete remote template.
-	 *
-	 * Remote template from Elementor.com servers cannot be deleted from the
-	 * database as they are retrieved from remote servers.
-	 *
-	 * @access public
-	 *
-	 * @param int $template_id The template ID.
-	 *
-	 * @return \WP_Error
-	 */
-	public function delete_template( $template_id ) {
-		return new \WP_Error( 'invalid_request', 'Cannot delete template from a remote source' );
-	}
+		if ( ha_has_pro() ) {
+			$body['has_pro'] = 1;
+			$body['pro_version'] = HAPPY_ADDONS_PRO_VERSION;
+		}
 
-	/**
-	 * Export remote template.
-	 *
-	 * Remote template from Elementor.com servers cannot be exported from the
-	 * database as they are retrieved from remote servers.
-	 *
-	 * @access public
-	 *
-	 * @param int $template_id The template ID.
-	 *
-	 * @return \WP_Error
-	 */
-	public function export_template( $template_id ) {
-		return new \WP_Error( 'invalid_request', 'Cannot export template from a remote source' );
+		$response = wp_remote_get(
+			self::API_TEMPLATE_DATA_URL . $template_id,
+			[
+				'body' => $body,
+				'timeout' => 25
+			]
+		);
+
+		return wp_remote_retrieve_body( $response );
 	}
 
 	/**
@@ -299,113 +199,27 @@ class Library_Source extends Source {
 	 *
 	 * Retrieve the data of a single remote template from Elementor.com servers.
 	 *
-	 * @access public
-	 *
-	 * @param array  $args    Custom template arguments.
-	 * @param string $context Optional. The context. Default is `display`.
-	 *
-	 * @return array Remote Template data.
+	 * @return array|\WP_Error Remote Template data.
 	 */
 	public function get_data( array $args, $context = 'display' ) {
-		$data = self::get_template_content( $args['template_id'] );
+		$data = self::request_template_data( $args['template_id'] );
 
-		if ( is_wp_error( $data ) ) {
-			return $data;
+		$data = json_decode( $data, true );
+
+		if ( empty( $data ) || empty( $data['content'] ) ) {
+			throw new \Exception( __( 'Template does not have any content', 'happy-elementor-addons' ) );
 		}
 
 		$data['content'] = $this->replace_elements_ids( $data['content'] );
 		$data['content'] = $this->process_export_import_content( $data['content'], 'on_import' );
 
 		$post_id = $args['editor_post_id'];
-		$document = Plugin::$instance->documents->get( $post_id );
+		$document = ha_elementor()->documents->get( $post_id );
+
 		if ( $document ) {
 			$data['content'] = $document->get_elements_raw_data( $data['content'], true );
 		}
 
 		return $data;
-	}
-
-	/**
-	 * Get template content.
-	 *
-	 * Retrieve the templates content received from a remote server.
-	 *
-	 * @access public
-	 * @static
-	 *
-	 * @param int $template_id The template ID.
-	 *
-	 * @return array The template content.
-	 */
-	public static function get_template_content( $template_id ) {
-		$url = sprintf( self::$api_get_template_content_url, $template_id );
-
-		$body_args = [
-			// Which API version is used.
-			'api_version' => ELEMENTOR_VERSION,
-			// Which language to return.
-			'site_lang' => get_bloginfo( 'language' ),
-		];
-
-		/**
-		 * API: Template body args.
-		 *
-		 * Filters the body arguments send with the GET request when fetching the content.
-		 *
-		 * @param array $body_args Body arguments.
-		 */
-		$body_args = apply_filters( 'elementor/api/get_templates/body_args', $body_args );
-
-		$response = wp_remote_get( $url, [
-			'timeout' => 40,
-			'body' => $body_args,
-		] );
-
-		if ( is_wp_error( $response ) ) {
-			return $response;
-		}
-
-		$response_code = (int) wp_remote_retrieve_response_code( $response );
-
-		if ( 200 !== $response_code ) {
-			return new \WP_Error( 'response_code_error', sprintf( 'The request returned with a status code of %s.', $response_code ) );
-		}
-
-		$template_content = json_decode( wp_remote_retrieve_body( $response ), true );
-
-		if ( isset( $template_content['error'] ) ) {
-			return new \WP_Error( 'response_error', $template_content['error'] );
-		}
-
-		if ( empty( $template_content['data'] ) && empty( $template_content['content'] ) ) {
-			return new \WP_Error( 'template_data_error', 'An invalid data was returned.' );
-		}
-
-		return $template_content;
-	}
-
-	/**
-	 * @access private
-	 */
-	private function prepare_template( array $template_data ) {
-		$favorite_templates = $this->get_user_meta( 'favorites' );
-
-		return [
-			'template_id' => $template_data['id'],
-			'source' => $this->get_id(),
-			'type' => $template_data['type'],
-			'subtype' => $template_data['subtype'],
-			'title' => $template_data['title'],
-			'thumbnail' => $template_data['thumbnail'],
-			'date' => $template_data['tmpl_created'],
-			'author' => $template_data['author'],
-			'tags' => json_decode( $template_data['tags'] ),
-			'isPro' => ( '1' === $template_data['is_pro'] ),
-			'popularityIndex' => (int) $template_data['popularity_index'],
-			'trendIndex' => (int) $template_data['trend_index'],
-			'hasPageSettings' => ( '1' === $template_data['has_page_settings'] ),
-			'url' => $template_data['url'],
-			'favorite' => ! empty( $favorite_templates[ $template_data['id'] ] ),
-		];
 	}
 }
