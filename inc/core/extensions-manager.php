9859462972cd2399bed5dee53f615391f3713d09
Saiful Islam <codersaiful@gmail.com>
2021-07-04 12:39:15 +0600

    create file extensions-array and included in entension-manager

--- inc/core/extensions-manager.php
+++ inc/core/extensions-manager.php
@@ -102,79 +102,20 @@ class Extensions_Manager{
      * @todo Ultra Effect is still not completed.
      */
     public static function get_list(){
-        return [
-            'Wrapper_Link'=> [
-                    'name'      => __( 'Wrapper Link', 'ultraaddons' ),
-                    'is_pro'   => false,
-                    'icon'      => 'uicon-button',
-                    'cat'       => [
-                        __( 'Basic', 'ultraaddons' ),
-                    ],
-            ],
+        /**
+         * File of Extension Array
+         */
+        $file = ULTRA_ADDONS_DIR . 'inc/core/list/extensions-array.php';
+        
+        if( ! is_file( $file ) ){
+            return [];
+        }
+        $extensionsArray = include $file;
 
-
-            'Hover_Effect' => [
-                    'name'  => __( 'Hover Effect', 'ultraaddons' ),
-                    'is_pro'   => false,
-                    'icon'      => 'uicon-hover',
-                    'cat'       => [
-                        __( 'Basic', 'ultraaddons' ),
-                    ],
-            ],
-            
-            /**
-             * CSS Transform Editing for
-             * Any content/any element/
-             * We will added this Extenstion at V1.0.3.0
-             * 
-             * @date 4.3.2021
-             * @since 1.0.3.0
-             */
-            'Transform' => [
-                    'name'  => __( 'CSS Transform', 'ultraaddons' ),
-                    'is_pro'   => true,
-                    'icon'      => 'eicon-heading',
-                    'cat'       => [
-                        __( 'Basic', 'ultraaddons' ),
-                    ],
-                    'tab' => Controls_Manager::TAB_STYLE, //This is only for PlaceHolder Extension. showing message on free user
-            ],
-            
-
-            'Animation_Effect' => [
-                    'name'  => __( 'Animation Effect', 'ultraaddons' ),
-                    'is_pro'   => true,
-                    'icon'      => 'eicon-code-highlight',
-                    'cat'       => [
-                        __( 'Basic', 'ultraaddons' ),
-                    ],
-                    'tab' => Controls_Manager::TAB_STYLE,  //This is only for PlaceHolder Extension. showing message on free user
-            ],
-
-            'Background_Overlay' => [
-                    'name'  => __( 'Background Overlay', 'ultraaddons' ),
-                    'is_pro'   => false,
-                    'icon'      => 'eicon-background',
-                    'cat'       => [
-                        __( 'Basic', 'ultraaddons' ),
-                    ],
-            ],
-
-            'Ultra_Effect' => [
-                    'name'  => __( 'Ultra Effect', 'ultraaddons' ),
-                    'is_pro'   => true,
-                    'icon'      => 'eicon-spinner',
-                    'cat'       => [
-                        __( 'Basic', 'ultraaddons' ),
-                    ],
-            ],
-
-//         Need more Customizer and to be update   
-//            'ultra-effects'=> [
-//                    'name'  => __( 'Ultra Effect', 'ultraaddons' ),
-//            ],
-            
-        ];
+        if( is_array( $extensionsArray ) ){
+            return $extensionsArray;
+        }
+        return [];
     }
     
     /**
