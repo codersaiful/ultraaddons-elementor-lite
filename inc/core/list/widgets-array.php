3ecebee40419b2b520189abcf8c3680453cfb955
Saiful Islam <codersaiful@gmail.com>
2021-08-10 16:11:05 +0600

    Hero_Banner added

--- inc/core/list/widgets-array.php
+++ inc/core/list/widgets-array.php
@@ -134,6 +134,15 @@ $widgetsArray = [
     ],
     
     
+    //New Added
+    'Hero_Banner' => [
+            'name'  => __( 'Hero Banner', 'ultraaddons' ),
+            'is_pro'   => false,
+            'icon'      => 'eicon-call-to-action',
+            'cat'       => [
+                __( 'Basic', 'ultraaddons' ),
+            ],
+    ],
     
     'Contact_Form7' => [
             'name'  => __( 'Contact Form 7', 'ultraaddons' ),
