e66f41c16773c0ffd51eb10f04193ef6076dc31b
Saiful Islam <codersaiful@gmail.com>
2021-08-10 12:47:40 +0600

    preset will try later

--- inc/extensions/preset.php
+++ inc/extensions/preset.php
@@ -43,7 +43,7 @@ class Preset {
 				'tab'   => $tabs,
 			]
 		);
-
+                
                 
                 $element->add_control(
                         '_ua_preset_name',
@@ -82,7 +82,7 @@ class Preset {
          * @param Element_Base $element
          */
 	public static function before_section_render( Element_Base $element ) {
-            var_dump($element->get_class_name());
+            
 		$settings = $element->get_settings_for_display();
                 //var_dump($settings);
                 $settings = array_filter( $settings );
