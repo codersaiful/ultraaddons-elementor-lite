a3b3dd9591c747406fb702aa7f373ece0de8a67d
codeastrology-dev <codeastrology.dev2@gmail.com>
2021-09-13 13:03:46 +0600

    working on welcome page

--- admin/pages/includes/admin-header.php
+++ admin/pages/includes/admin-header.php
@@ -54,45 +54,6 @@ $full_logo_image    = apply_filters( 'ultraaddons/admin/header_logo', $full_logo
                 </div>
             </div>
 
-            <div class="ua-admin-welcome-content-area">
-                <section class="welcome-banner" style="background-image: url(<?php echo esc_url( ULTRA_ADDONS_ASSETS . 'images/header-background.png' ); ?>);">
-                    <img src="<?php echo esc_url( ULTRA_ADDONS_ASSETS . 'images/transparent-logo.png' ); ?>" alt="" class="logo" />
-                    <p class="greetings"><?php echo esc_html__( 'Thanks for choosing us!', 'ultraaddons' )?></p>
-                </section>
-
-                <section class="ua-section pr1">
-                    <div class="info-box">
-                        <div class="icon">
-                            <img src="<?php echo esc_url( ULTRA_ADDONS_ASSETS . 'images/svg-icon/file.svg' ); ?>" alt="" class="icon" />
-                        </div>
-                        <div class="info">
-                            <h3><?php echo esc_html__( 'User Guidelines', 'ultraaddons' ); ?></h3>
-                            <p><?php echo esc_html__( 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words', 'ultraaddons' ); ?></p>
-                            <a href="#" class="ua-button button"><?php echo esc_html__( 'Documentation', 'ultraaddons' ); ?></a>
-                        </div>
-                    </div>
-                    <div class="info-box">
-                        <div class="icon">
-                            <img src="<?php echo esc_url( ULTRA_ADDONS_ASSETS . 'images/svg-icon/headphones.svg' ); ?>" alt="" class="icon" />
-                        </div>
-                        <div class="info">
-                            <h3><?php echo esc_html__( 'Do you have any query?', 'ultraaddons' ); ?></h3>
-                            <p><?php echo esc_html__( 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words', 'ultraaddons' ); ?></p>
-                            <a href="#" class="ua-button button"><?php echo esc_html__( 'Contact Support', 'ultraaddons' ); ?></a>
-                        </div>
-                    </div>
-
-                </section>
-
-                <section class="ua-section pr2">
-                    <div class="full-width info-box">
-                        <h3 class="big"><?php echo esc_html__( 'Missing Any Features?', 'ultraaddons' ); ?></h3>
-                        <p><?php echo esc_html__( 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words', 'ultraaddons' ); ?></p>
-                        <a href="#" class="ua-button button"><?php echo esc_html__( 'Request For Features', 'ultraaddons' ); ?></a>
-                    </div>
-                </section>
-
-            </div>
         
 <?php 
 
