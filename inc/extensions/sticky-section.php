0c77698fc55769a739e822ce0650ff2af228d3ae
Saiful Islam <codersaiful@gmail.com>
2021-11-22 12:27:23 +0600

    Sticky Section Extension finalized

--- inc/extensions/sticky-section.php
+++ inc/extensions/sticky-section.php
@@ -4,18 +4,45 @@ namespace UltraAddons\Extensions;
 
 use Elementor\Controls_Manager;
 use Elementor\Element_Base;
-use Elementor\Group_Control_Background;
 
 
 defined('ABSPATH') || die();
 
+/**
+ * Sticky Secttion 
+ * 
+ * ************************
+ * EXTENSION
+ * ************************
+ * 
+ * will be available for only section actually
+ * 
+ * we can add this feature for any element at later
+ * 
+ * @credit https://www.w3schools.com/howto/howto_css_sticky_element.asp
+ * 
+ * @package UltraAddons
+ * @since 1.1.0.7
+ * @author Saiful islam <codersaiful@gmail.com>
+ */
 class Sticky_Section{
 
+    /**
+     * Initialize Sticky section
+     *
+     * @return void
+     */
     public static function init(){
         add_action( 'elementor/element/section/section_advanced/after_section_end', [ __CLASS__, 'add_controls_section' ], 1 );
         //add_action( 'elementor/element/common/_section_style/after_section_end', [ __CLASS__, 'add_controls_section' ], 1 );
     }
 
+    /**
+     * Adding control for Sticky Section Extension
+     *
+     * @param Element_Base $element
+     * @return void
+     */
     public static function add_controls_section( Element_Base $element ){
 
         //Only for section, this feature will activating
@@ -43,12 +70,35 @@ class Sticky_Section{
                     'default' => '',
                     'prefix_class' => 'ua-sticky-',
                     'selectors' => [
-                        '{{WRAPPER}}.ua-sticky-yes' => 'opacity: 0.33;',
-    //					'{{WRAPPER}} > .elementor-element-populated >  .elementor-background-overlay' => 'opacity: {{SIZE}};',
+                        '{{WRAPPER}}.ua-sticky-yes' => 'position: sticky;width: 100%;z-index: 999;',
                     ],
             ]
         );
-
+        $element->add_responsive_control(
+            '_ua_sticky_scroll_top',
+            [
+                'label' => esc_html__('Scroll Top', 'ultraaddons'),
+                'type' => Controls_Manager::SLIDER,
+                'default' => [
+                    'size' => 0,
+                    'unit' => 'px',
+                ],
+                'size_units' => ['px','%'],
+                'range' => [
+                    'px' => [
+                        'min' => 0,
+                        'max' => 300,
+                        'step' => 1,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}}.ua-sticky-yes' => 'top: {{SIZE}}{{UNIT}};',
+                ],
+                'condition' => [
+                    '_ua_sticky_section_switch' => 'yes'
+                ]
+            ]
+        );
 
         $element->end_controls_section();
         
