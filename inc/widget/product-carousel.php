014e001e7db85d938a23d90a60c3c378b36b3909
rafiul <bmrafiul.alam@gmail.com>
2021-12-02 14:19:48 +0600

    Product carousel Adding options

--- inc/widget/product-carousel.php
+++ inc/widget/product-carousel.php
@@ -97,30 +97,182 @@ class Product_Carousel extends Base{
 
         //For General Section
         $this->content_general_controls();
+        //For Query Tab
+        $this->query_controls();
+        //For General Style Tab
+        $this->style_general_controls();
     }
+
+    protected function query_controls() {
+		
+        $this->start_controls_section(
+            'query_content',
+            [
+                'label'     => esc_html__( 'Query Settings', 'ultraaddons' ),
+                'tab'       => Controls_Manager::TAB_CONTENT,
+            ]
+        );
+		$this->add_control(
+			'_ua_text_truncate',
+			[
+				'label' => __( 'Description Length', 'ultraaddons' ),
+				'type' => Controls_Manager::NUMBER,
+				'min' => 5,
+				'max' => 300,
+				'step' => 5,
+				'default' => 10,
+			]
+		);
+		$this->add_control(
+			'_ua_post_per_page',
+			[
+				'label' => __( 'Show Products', 'ultraaddons' ),
+				'type' => Controls_Manager::NUMBER,
+				'min' => 1,
+				'max' => 300,
+				'step' => 1,
+				'default' => 6,
+			]
+		);
+		
+		$this->add_control(
+			'_ua_post_page_number',
+			[
+				'label' => __( 'Page Number', 'ultraaddons' ),
+				'type' => Controls_Manager::NUMBER,
+				'min' => 1,
+				//'max' => 300,
+				'step' => 1,
+				'default' => 1,
+			]
+		);
+
+		$this->add_control(
+			'_ua_product_order',
+			[
+				'label' => esc_html__( 'Order', 'ultraaddons' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'asc' => 'Asc',
+					'desc' => 'Desc',
+				],
+				'default' => 'desc',
+			]
+		);
+		$this->add_control(
+			'_ua_product_orderby',
+			[
+				'label' => esc_html__( 'Orderby', 'ultraaddons' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'none' => 'None',
+					'id' => 'ID',
+					'name' => 'Name',
+					'type' => 'Type',
+					'rand' => 'Random',
+					'date' => 'Date',
+					'modified' => 'Modified',
+				],
+				'default' => 'date',
+			]
+		);
+
+		$this->add_control(
+            'cat_ids',
+            [
+                'label' => esc_html__( 'Select category', 'ultraaddons' ),
+                'type' => Controls_Manager::SELECT2,
+                'options' => $this->product_tax_options(),
+                'multiple' => 'true'
+            ]
+        );
+
+		
+		$this->add_control(
+            'tag_ids',
+            [
+                'label' => esc_html__( 'Select Tag', 'ultraaddons' ),
+                'type' => Controls_Manager::SELECT2,
+                'options' => $this->product_tax_options( 'product_tag' ),
+                'multiple' => 'true'
+            ]
+        );
+
+
+		$this->add_control(
+			'_ua_query_post_in',
+			[
+				'label' => __( 'Product by included IDs', 'ultraaddons' ),
+				'type' => Controls_Manager::TEXTAREA,
+				'placeholder' => __( '1,2,3,4,20,33', 'ultraaddons' ),
+				'description' => __('Add multiple ids by comma separated.'),
+				'label_block' => true,
+			]
+		);
+		$this->add_control(
+			'_ua_query_post_not_in',
+			[
+				'label' => __( 'Product by excluded IDs', 'ultraaddons' ),
+				'type' => Controls_Manager::TEXTAREA,
+				'placeholder' => __( '1,2,3,4,20,33', 'ultraaddons' ),
+				'description' => __('Add multiple ids by comma separated.'),
+				'label_block' => true,
+			]
+		);
+		 
+		
+		$this->end_controls_section();
+	}
+
     
     /**
      * General Section for Content Controls
      * 
      * @since 1.0.0.9
      */
-    protected function content_general_controls() {
-
+  protected function content_general_controls() {
+		
         $this->start_controls_section(
             'general_content',
             [
-                'label'     => esc_html__( 'General', 'ultraaddons' ),
+                'label'     => esc_html__( 'Slider Options', 'ultraaddons' ),
                 'tab'       => Controls_Manager::TAB_CONTENT,
             ]
         );
-        
-       
-    
-        $this->end_controls_section();
-    }
+
+		 $this->add_control(
+			'_slider_to_show',
+			[
+				'label' => esc_html__( 'Slider to Show', 'ultraaddons' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'1' => 'One',
+					'2' => 'Two',
+					'3' => 'Three',
+					'4' => 'Four',
+					'5' => 'Five',
+					'6' => 'Six',
+				],
+				'default' => '3',
+			]
+		);
+        $this->add_control(
+			'_slider_gap',
+			[
+				'label' => __( 'Gap', 'ultraaddons' ),
+				'type' => Controls_Manager::SWITCHER,
+				'label_on' => __( 'Yes', 'ultraaddons' ),
+				'label_off' => __( 'No', 'ultraaddons' ),
+				'return_value' => 'yes',
+				'default' => 'yes',
+			]
+		);
+		
+	$this->end_controls_section();
+	}
     
     /**
-     * Alignment Section for Style Tab
+     *  Section for Style Tab
      * 
      * @since 1.0.0.9
      */
@@ -132,6 +284,24 @@ class Product_Carousel extends Base{
                 'tab'       => Controls_Manager::TAB_STYLE,
             ]
         );
+        $this->add_control(
+			'_ua_front_title_tag',
+			[
+				'label' => esc_html__( 'Select Front Title Tag', 'ultraaddons' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'h1' => 'H1',
+					'h2' => 'H2',
+					'h3' => 'H3',
+					'h4' => 'H4',
+					'h5' => 'H5',
+					'h6' => 'H6',
+					'div' => 'div',
+				],
+				'default' => 'h2',
+			]
+		);
+		
         
        
     $this->end_controls_tab();
@@ -147,26 +317,111 @@ class Product_Carousel extends Base{
      * @access protected
      */
     protected function render() {
-
-    $settings = $this->get_settings_for_display();
+    //Intrigate with WooCommerce
+    if( ! class_exists( 'WooCommerce' ) ){
+        echo "<div class='ua-alert'>" . esc_html__( "WooCommerce is not Activated.", 'ultraaddons' ) . "</div>";
+        return;
+    }
+    $settings   = $this->get_settings_for_display();
+    $gap        = ($settings['_slider_gap']=='yes') ? '' : ' slider-item-nogap';
+    $to_show    = $settings['_slider_to_show'] ? ' slider-item-show'. $settings['_slider_to_show'] : ''  ;
+    
+    $this->add_render_attribute(
+		'slider_options',
+		[
+			'class' => 'swiffy-slider '. $to_show . $gap ,
+		]
+	);
     ?>
 
-<div class="swiffy-slider slider-item-show3 slider-item-reveal slider-nav-dark slider-nav-outside-expand">
+<div <?php echo $this->get_render_attribute_string( 'slider_options' ); ?>>
     <ul class="slider-container py-4" id="slider2">
+        <?php
+		
+        $args = array(
+            'post_type' 	=> 'product',
+            'posts_per_page'=> $settings['_ua_post_per_page'],
+            'paged'=> ! empty( $settings['_ua_post_page_number'] ) ? $settings['_ua_post_page_number'] : 1,
+			'order'			=> $settings['_ua_product_order'],
+			'orderby'		=> $settings['_ua_product_orderby'],
+            );
+		if(! empty( $settings['_ua_query_post_in'] )){
+			$include_ids = explode(',',$settings['_ua_query_post_in']);
+			$args['post__in'] = $include_ids;
+		}
+		if(! empty( $settings['_ua_query_post_not_in'] )){
+			$exclude_ids = explode(',',$settings['_ua_query_post_not_in']);
+			$args['post__not_in'] = $exclude_ids;
+		}
+
+		if( ! empty( $settings['cat_ids'] ) ){
+			$args['tax_query'] = array(
+				array(
+					'taxonomy'  => 'product_cat',
+					'field'     => 'id', 
+					'terms'     => $settings['cat_ids'],
+				)
+			);
+		}	
+
+		
+		if( ! empty( $settings['tag_ids'] ) ){
+			$args['tax_query'] = array(
+				array(
+					'taxonomy'  => 'product_tag',
+					'field'     => 'id', 
+					'terms'     => $settings['tag_ids'],
+				)
+			);
+		}	
+
+        $loop = new \WP_Query( $args );
+        if ( $loop->have_posts() ) {
+            while ( $loop->have_posts() ) : $loop->the_post();
+				$id 		= $loop->post->ID;
+                $product   	= wc_get_product( $id );
+				$image_id  	= $product->get_image_id();
+				$image_url 	= wp_get_attachment_image_url( $image_id, 'full' );
+				$description = $loop->post->post_excerpt;
+                $category = get_the_category( $id );
+                if(!empty($category)){
+                    echo $category[0]->cat_name;
+                }
+    ?>
         <li>
             <div class="ua-card shadow ua-h-100">
-                <div class="ratio ratio-1x1">
-                    <img src="https://www.cssscript.com/demo/versatile-carousel-swiffy-slider/docs/assets/img/shoes/shoe1.webp" class="card-img-top" loading="lazy" alt="...">
+                <div class="ua-thumbnail">
+                    <?php echo woocommerce_get_product_thumbnail('woocommerce_full_size');?>
                 </div>
                 <div class="ua-card-body ua-d-flex ua-flex-column ua-flex-md-row">
                     <div class="ua-flex-grow-1">
-                        <strong>LeBron 18</strong>
-                        <p class="ua-card-text">Basketball Shoes</p>
+                        <a href="<?php echo get_the_permalink(); ?>">
+                            <?php
+                            echo '<' . $settings['_ua_front_title_tag'] . ' class="front-title">' . $loop->post->post_title . 
+                                    '</' . $settings['_ua_front_title_tag'] . '>';
+                            ?>
+                        </a>
+                        <p class="ua-card-text">
+                            <?php 
+                            foreach( wp_get_post_terms( get_the_id(), 'product_cat' ) as $term ){
+                            if( $term ){
+                                    echo $term->name; // product category name
+                                }
+                            }
+                            ?>
+                        </p>
                     </div>
-                    <div class="ua-px-md-2">$200</div>
+                    <div class="ua-px-md-2"><?php echo $product->get_price_html();?> </div>
                 </div>
             </div>
         </li>
+        <?php
+	 endwhile;
+	} else {
+		 echo "<div class='ua-alert'>" . esc_html__( "No products found!", 'ultraaddons' ) . "</div>";
+	}
+	wp_reset_postdata();
+	?>
     </ul>
     <button type="button" class="slider-nav" aria-label="Go to previous"></button>
     <button type="button" class="slider-nav slider-nav-next" aria-label="Go to next"></button>
@@ -174,4 +429,29 @@ class Product_Carousel extends Base{
 <?php
         
     }
+
+    /**
+     * Getting Category list of WooCommerce product
+     * 
+     *
+     * @return void
+     */
+    public function product_tax_options( $taxonomy = 'product_cat' ) {
+        $query_args = array(
+            'orderby'       => 'ID',
+            'order'         => 'DESC',
+            'hide_empty'    => false,
+            'taxonomy'      => $taxonomy
+        );
+
+        $categories = get_categories( $query_args );
+        $options = array();
+        if(is_array($categories) && count($categories) > 0){
+            foreach ($categories as $cat){
+                $options[$cat->term_id] = $cat->name;
+            }
+            return $options;
+        }
+    }
+
 }
