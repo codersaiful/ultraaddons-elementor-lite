5426dfe9100215b5237d0775b866b292e07d1ddb
Saiful Islam <codersaiful@gmail.com>
2021-12-02 15:22:28 +0600

    working

--- inc/widget/image-hover-3d.php
+++ inc/widget/image-hover-3d.php
@@ -7,7 +7,6 @@ use Elementor\Group_Control_Typography;
 use Elementor\Core\Schemes\Typography;
 use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
 use Elementor\Group_Control_Border;
-use Elementor\Group_Control_Box_Shadow;
 use Elementor\Group_Control_Background;
 use Elementor\Repeater;
 
@@ -25,7 +24,7 @@ class Image_Hover_3D extends Base{
      * @return string keywords
      */
     public function get_keywords() {
-        return [ 'ultraaddons', '3d', 'image', 'magic', 'slide'];
+        return [ 'ultraaddons','ua', '3d', 'image', 'magic', 'slide'];
     }
     
     
@@ -41,7 +40,7 @@ class Image_Hover_3D extends Base{
         
 
         //For General Section
-        //$this->content_general_controls();
+        //$this->ua-3dimage-content_general_controls();
 
     
        
@@ -59,9 +58,55 @@ class Image_Hover_3D extends Base{
         $settings           = $this->get_settings_for_display();
 
         ?>
-        <div>
-            
-            
+        <div class="ua-3d-image-hover-wrapper">
+            <div class="ua-3dimage-box">
+                <div class="ua-3dimage-imgbx">
+                <img src="https://images.unsplash.com/photo-1579748138140-ce9ef2c32db1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
+                </div>
+                <div class="ua-3dimage-content">
+                <div>
+                    <h2>Image Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div class="ua-3dimage-box">
+                <div class="ua-3dimage-imgbx">
+                <img src="https://images.unsplash.com/photo-1579639782539-15cc6c0be63f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
+                </div>
+                <div class="ua-3dimage-content">
+                <div>
+                    <h2>Image Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div class="ua-3dimage-box">
+                <div class="ua-3dimage-imgbx">
+                <img src="https://images.unsplash.com/photo-1603984362497-0a878f607b92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80">
+                </div>
+                <div class="ua-3dimage-content">
+                <div>
+                    <h2>Image Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div class="ua-3dimage-box">
+                <div class="ua-3dimage-imgbx">
+                <img src="https://images.unsplash.com/photo-1579310962131-aa21f240d986?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80">
+                </div>
+                <div class="ua-3dimage-content">
+                <div>
+                    <h2>Image Title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                </div>
+                </div>
+            </div>
         </div>
         <?php
         
