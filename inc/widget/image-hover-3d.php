18e59c11528b4405f98b014349c8ff91b11bfcd7
Saiful Islam <codersaiful@gmail.com>
2021-12-02 15:50:27 +0600

    adding control to image hover 3d

--- inc/widget/image-hover-3d.php
+++ inc/widget/image-hover-3d.php
@@ -9,6 +9,7 @@ use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
 use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Background;
 use Elementor\Repeater;
+use Elementor\Utils;
 
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -24,7 +25,7 @@ class Image_Hover_3D extends Base{
      * @return string keywords
      */
     public function get_keywords() {
-        return [ 'ultraaddons','ua', '3d', 'image', 'magic', 'slide'];
+        return [ 'ultraaddons','ua', '3d', 'image', 'magic', '3dimage','magic image'];
     }
     
     
@@ -40,76 +41,153 @@ class Image_Hover_3D extends Base{
         
 
         //For General Section
-        //$this->ua-3dimage-content_general_controls();
+        $this->content_general_controls();
 
     
        
     }
     
+
+
     /**
      * Render whole project
      *
-     * @since 1.0.0
+     * @since 1.1.0.8
      * @access protected
      */
     protected function render() {
 
         
         $settings           = $this->get_settings_for_display();
+        
+
+        if(empty($settings['images'])) return;
 
         ?>
         <div class="ua-3d-image-hover-wrapper">
-            <div class="ua-3dimage-box">
+            <?php
+            foreach($settings['images'] as $image){
+            ?>
+            <div class="ua-3dimage-box 3dimage-box-<?php echo $image['_id']; ?>">
                 <div class="ua-3dimage-imgbx">
-                <img src="https://images.unsplash.com/photo-1579748138140-ce9ef2c32db1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
+                    <img src="<?php echo esc_url( $image['image']['url'] ); ?>">
                 </div>
                 <div class="ua-3dimage-content">
-                <div>
-                    <h2>Image Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                    </p>
-                </div>
-                </div>
-            </div>
-            <div class="ua-3dimage-box">
-                <div class="ua-3dimage-imgbx">
-                <img src="https://images.unsplash.com/photo-1579639782539-15cc6c0be63f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
-                </div>
-                <div class="ua-3dimage-content">
-                <div>
-                    <h2>Image Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                    </p>
-                </div>
+                    <div class="ua-3dimage-content-inside">
+                        <h2><?php echo $image['title']; ?></h2>
+                        <p><?php echo $image['content']; ?></p>
+                    </div>
                 </div>
             </div>
-            <div class="ua-3dimage-box">
-                <div class="ua-3dimage-imgbx">
-                <img src="https://images.unsplash.com/photo-1603984362497-0a878f607b92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80">
-                </div>
-                <div class="ua-3dimage-content">
-                <div>
-                    <h2>Image Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                    </p>
-                </div>
-                </div>
-            </div>
-            <div class="ua-3dimage-box">
-                <div class="ua-3dimage-imgbx">
-                <img src="https://images.unsplash.com/photo-1579310962131-aa21f240d986?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80">
-                </div>
-                <div class="ua-3dimage-content">
-                <div>
-                    <h2>Image Title</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
-                    </p>
-                </div>
-                </div>
-            </div>
+            <?php
+            }
+            ?>
+            
+            
         </div>
         <?php
         
+    }
+
+
+    /**
+     * General Section for Content Controls
+     * 
+     * @since 1.0.0.9
+     */
+    protected function content_general_controls() {
+    
+        $this->start_controls_section(
+            'general',
+            [
+                'label' => __( 'General', 'ultraaddons' ),
+            ]
+        );
+
+        $repeater = new Repeater();
+        $repeater->add_control(
+                'image',
+                [
+                        'label' => __( 'Background Image', 'ultraaddons' ),
+                        'type' => Controls_Manager::MEDIA,
+                        'default' => [
+                                'url' => Utils::get_placeholder_image_src(),
+                        ],
+                        'dynamic' => [
+                                'active' => true,
+                        ],
+
+                ]
+        );
+        $repeater->add_control(
+                'title',
+                [
+                    'label' => __( 'Title', 'ultraaddons' ),
+                    'type' => Controls_Manager::TEXT,
+                    'dynamic' => [
+                        'active' => true,
+                    ],
+                    'placeholder' => __( 'Enter your title', 'ultraaddons' ),
+                    'default' => __( 'Slider Title Text', 'ultraaddons' ),
+                    'label_block' => true,
+                ]                                       
+        );
+        
+        $repeater->add_control(
+                'content',
+                [
+                    'label' => __( 'Content', 'ultraaddons' ),
+                    'type' => Controls_Manager::TEXT,
+                    'dynamic' => [
+                        'active' => true,
+                    ],
+                    'placeholder' => __( 'Enter your content...', 'ultraaddons' ),
+                    'default' => __( "Lorem Ipsum is simply dumy text of the printing & typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1975, when an unknown printer.", 'ultraaddons' ),
+                    'label_block' => true,
+                ]
+        );
+
+        $this->add_control(
+                'images',
+                [
+                    'label' => __( 'Images', 'ultraaddons' ),
+                    'type' => Controls_Manager::REPEATER,
+                    'fields' => $repeater->get_controls(),
+                    'default' => [
+                        [
+                            'image' => Utils::get_placeholder_image_src(),
+                            'title' => __( 'Image Title', 'ultraaddons' ),
+                            'content' => __( 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.', 'ultraaddons' ),
+                                                    
+                        ],
+                                        
+                        [
+                            'image' => Utils::get_placeholder_image_src(),
+                            'title' => __( 'Image Title', 'ultraaddons' ),
+                            'content' => __( 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.', 'ultraaddons' ),
+                                                    
+                        ],
+                                        
+                        [
+                            'image' => Utils::get_placeholder_image_src(),
+                            'title' => __( 'Image Title', 'ultraaddons' ),
+                            'content' => __( 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.', 'ultraaddons' ),
+                                                    
+                        ],
+                                        
+                        [
+                            'image' => Utils::get_placeholder_image_src(),
+                            'title' => __( 'Image Title', 'ultraaddons' ),
+                            'content' => __( 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.', 'ultraaddons' ),
+                                                    
+                        ],
+                                        
+                        
+                    ],
+                    'title_field' => '{{{ title }}}',
+                ]
+            );
+            $this->end_controls_section();
     }
     
     
