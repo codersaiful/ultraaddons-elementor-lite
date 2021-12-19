1a24a1c93bf81e8e28ce381a8bf29994e913b0f4
rafiul7 <bmrafiul.alam@gmail.com>
2021-12-19 17:03:08 +0600

    Moving letter js load problem solved

--- inc/widget/moving-letters.php
+++ inc/widget/moving-letters.php
@@ -309,7 +309,45 @@ class Moving_Letters extends Base{
         $anim_type = $settings['anim_type'];
         // var_dump($anim_type);
         ?>
+<<<<<<< HEAD
     
+=======
+        <?php
+            if( Plugin::$instance->editor->is_edit_mode()){
+                ?>
+                <script>
+                var textWrapper = document.querySelector('.ml1 .letters');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                    anime.timeline({loop: true})
+                    .add({
+                        targets: '.ml1 .letter',
+                        scale: [0.3,1],
+                        opacity: [0,1],
+                        translateZ: 0,
+                        easing: "easeOutExpo",
+                        duration: 600,
+                        delay: (el, i) => 70 * (i+1)
+                    }).add({
+                        targets: '.ml1 .line',
+                        scaleX: [0,1],
+                        opacity: [0.5,1],
+                        easing: "easeOutExpo",
+                        duration: 700,
+                        offset: '-=875',
+                        delay: (el, i, l) => 80 * (l - i)
+                    }).add({
+                        targets: '.ml1',
+                        opacity: 0,
+                        duration: 1000,
+                        easing: "easeOutExpo",
+                        delay: 1000
+                    });
+                </script>
+                <?php
+            }
+        ?>
+>>>>>>> 5ca6f6579808570ecf8e6aa3f01c0bd582ed7798
         <?php
         if($anim_type==1){
         echo' <h1 class="ua-ml ml1">
