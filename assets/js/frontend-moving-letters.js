5ca6f6579808570ecf8e6aa3f01c0bd582ed7798
Saiful Islam <codersaiful@gmail.com>
2021-12-19 15:36:07 +0600

    moving letters issue fixed

--- assets/js/frontend-moving-letters.js
+++ assets/js/frontend-moving-letters.js
@@ -11,22 +11,22 @@ $window.on( 'elementor/frontend/init', function() {
     
     var ModuleBase = elementorModules.frontend.handlers.Base;
     var CusttomCSS;
-            /**
-             * Moving Letters Finalized Here
-             * 
-             * Actually most of the part of this Widget has done by Numan but
-             * JS part had developed by(Me) Saiful Islam
-             * 
-             * @author B M Rafiul Alam <bmrafiul.alam@gmail.com>
-             * @since 1.1.0.8
-             */
 
-            // Moving_Letters Hooked Here
-            EF.hooks.addAction(
-                'frontend/element_ready/ultraaddons-moving-letters.default',
-                function ($scope) {
-                    if ( $scope.find( '.ua-ml' ) ){
-                     var anim_data = $scope[0].dataset.settings;
+
+    var Moving_Letters = EM.frontend.handlers.Base.extend({
+        onInit: function(){
+            this.run();
+        },
+        onChange: function(){
+            this.run();
+        },
+        run: function(){
+
+            var $scope = this.$element;
+            var targetElement = this.$element.find('.ua-ml');
+           
+            
+            var anim_data = $scope[0].dataset.settings;
                     if(anim_data){
                         var obj = JSON.parse(anim_data);
                     }
@@ -60,6 +60,7 @@ $window.on( 'elementor/frontend/init', function() {
                         delay: 1000
                     });
                 }
+
                 if(typeof(obj)!= "undefined" && obj.anim_type==2){
                     var textWrapper = document.querySelector('.ml2');
                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -82,7 +83,7 @@ $window.on( 'elementor/frontend/init', function() {
                     });
                 }
                 if(typeof(obj)!= "undefined" &&  obj.anim_type==3){
-                   // Wrap every letter in a span
+                // Wrap every letter in a span
                     var textWrapper = document.querySelector('.ml3');
                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -112,110 +113,110 @@ $window.on( 'elementor/frontend/init', function() {
                     ml4.delay = 500;
                     
                     anime.timeline({loop: true})
-                      .add({
+                    .add({
                         targets: '.ml4 .letters-1',
                         opacity: ml4.opacityIn,
                         scale: ml4.scaleIn,
                         duration: ml4.durationIn
-                      }).add({
+                    }).add({
                         targets: '.ml4 .letters-1',
                         opacity: 0,
                         scale: ml4.scaleOut,
                         duration: ml4.durationOut,
                         easing: "easeInExpo",
                         delay: ml4.delay
-                      }).add({
+                    }).add({
                         targets: '.ml4 .letters-2',
                         opacity: ml4.opacityIn,
                         scale: ml4.scaleIn,
                         duration: ml4.durationIn
-                      }).add({
+                    }).add({
                         targets: '.ml4 .letters-2',
                         opacity: 0,
                         scale: ml4.scaleOut,
                         duration: ml4.durationOut,
                         easing: "easeInExpo",
                         delay: ml4.delay
-                      }).add({
+                    }).add({
                         targets: '.ml4 .letters-3',
                         opacity: ml4.opacityIn,
                         scale: ml4.scaleIn,
                         duration: ml4.durationIn
-                      }).add({
+                    }).add({
                         targets: '.ml4 .letters-3',
                         opacity: 0,
                         scale: ml4.scaleOut,
                         duration: ml4.durationOut,
                         easing: "easeInExpo",
                         delay: ml4.delay
-                      }).add({
+                    }).add({
                         targets: '.ml4',
                         opacity: 0,
                         duration: 500,
                         delay: 500
-                      });
+                    });
                 }
                 if(typeof(obj)!= "undefined"&&  obj.anim_type==5){
                     anime.timeline({loop: true})
                     .add({
-                      targets: '.ml5 .line',
-                      opacity: [0.5,1],
-                      scaleX: [0, 1],
-                      easing: "easeInOutExpo",
-                      duration: 700
+                    targets: '.ml5 .line',
+                    opacity: [0.5,1],
+                    scaleX: [0, 1],
+                    easing: "easeInOutExpo",
+                    duration: 700
                     }).add({
-                      targets: '.ml5 .line',
-                      duration: 600,
-                      easing: "easeOutExpo",
-                      translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+                    targets: '.ml5 .line',
+                    duration: 600,
+                    easing: "easeOutExpo",
+                    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
                     }).add({
-                      targets: '.ml5 .ampersand',
-                      opacity: [0,1],
-                      scaleY: [0.5, 1],
-                      easing: "easeOutExpo",
-                      duration: 600,
-                      offset: '-=600'
+                    targets: '.ml5 .ampersand',
+                    opacity: [0,1],
+                    scaleY: [0.5, 1],
+                    easing: "easeOutExpo",
+                    duration: 600,
+                    offset: '-=600'
                     }).add({
-                      targets: '.ml5 .letters-left',
-                      opacity: [0,1],
-                      translateX: ["0.5em", 0],
-                      easing: "easeOutExpo",
-                      duration: 600,
-                      offset: '-=300'
+                    targets: '.ml5 .letters-left',
+                    opacity: [0,1],
+                    translateX: ["0.5em", 0],
+                    easing: "easeOutExpo",
+                    duration: 600,
+                    offset: '-=300'
                     }).add({
-                      targets: '.ml5 .letters-right',
-                      opacity: [0,1],
-                      translateX: ["-0.5em", 0],
-                      easing: "easeOutExpo",
-                      duration: 600,
-                      offset: '-=600'
+                    targets: '.ml5 .letters-right',
+                    opacity: [0,1],
+                    translateX: ["-0.5em", 0],
+                    easing: "easeOutExpo",
+                    duration: 600,
+                    offset: '-=600'
                     }).add({
-                      targets: '.ml5',
-                      opacity: 0,
-                      duration: 1000,
-                      easing: "easeOutExpo",
-                      delay: 1000
+                    targets: '.ml5',
+                    opacity: 0,
+                    duration: 1000,
+                    easing: "easeOutExpo",
+                    delay: 1000
                     });
-                  }
+                }
                 if(typeof(obj)!= "undefined"&&  obj.anim_type==6){
                     // Wrap every letter in a span
-                      var textWrapper = document.querySelector('.ml6 .letters');
-                      textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-  
-                      anime.timeline({loop: true})
-                      .add({
-                          targets: '.ml6 .letter',
-                          translateY: ["1.1em", 0],
-                          translateZ: 0,
-                          duration: 750,
-                          delay: (el, i) => 50 * i
-                      }).add({
-                          targets: '.ml6',
-                          opacity: 0,
-                          duration: 1000,
-                          easing: "easeOutExpo",
-                          delay: 1000
-                      });
+                    var textWrapper = document.querySelector('.ml6 .letters');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                    anime.timeline({loop: true})
+                    .add({
+                        targets: '.ml6 .letter',
+                        translateY: ["1.1em", 0],
+                        translateZ: 0,
+                        duration: 750,
+                        delay: (el, i) => 50 * i
+                    }).add({
+                        targets: '.ml6',
+                        opacity: 0,
+                        duration: 1000,
+                        easing: "easeOutExpo",
+                        delay: 1000
+                    });
                 }
                 if(typeof(obj)!= "undefined" &&  obj.anim_type==7){
                     var textWrapper = document.querySelector('.ml7 .letters');
@@ -289,45 +290,45 @@ $window.on( 'elementor/frontend/init', function() {
                 }
                 if(typeof(obj)!= "undefined" &&  obj.anim_type==9){
                     // Wrap every letter in a span
-                     var textWrapper = document.querySelector('.ml9 .letters');
-                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
- 
-                     anime.timeline({loop: true})
-                     .add({
-                         targets: '.ml9 .letter',
-                         scale: [0, 1],
-                         duration: 1500,
-                         elasticity: 600,
-                         delay: (el, i) => 45 * (i+1)
-                     }).add({
-                         targets: '.ml9',
-                         opacity: 0,
-                         duration: 1000,
-                         easing: "easeOutExpo",
-                         delay: 1000
-                     });
-                 }
-                 if(typeof(obj)!= "undefined" &&  obj.anim_type==10){
-                   // Wrap every letter in a span
-                     var textWrapper = document.querySelector('.ml10 .letters');
-                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
- 
-                     anime.timeline({loop: true})
-                     .add({
-                         targets: '.ml10 .letter',
-                         rotateY: [-90, 0],
-                         duration: 1300,
-                         delay: (el, i) => 45 * i
-                     }).add({
-                         targets: '.ml10',
-                         opacity: 0,
-                         duration: 1000,
-                         easing: "easeOutExpo",
-                         delay: 1000
-                     });
-                 }
-                 if(typeof(obj)!= "undefined" &&  obj.anim_type==11){
-                 // Wrap every letter in a span
+                    var textWrapper = document.querySelector('.ml9 .letters');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                    anime.timeline({loop: true})
+                    .add({
+                        targets: '.ml9 .letter',
+                        scale: [0, 1],
+                        duration: 1500,
+                        elasticity: 600,
+                        delay: (el, i) => 45 * (i+1)
+                    }).add({
+                        targets: '.ml9',
+                        opacity: 0,
+                        duration: 1000,
+                        easing: "easeOutExpo",
+                        delay: 1000
+                    });
+                }
+                if(typeof(obj)!= "undefined" &&  obj.anim_type==10){
+                // Wrap every letter in a span
+                    var textWrapper = document.querySelector('.ml10 .letters');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                    anime.timeline({loop: true})
+                    .add({
+                        targets: '.ml10 .letter',
+                        rotateY: [-90, 0],
+                        duration: 1300,
+                        delay: (el, i) => 45 * i
+                    }).add({
+                        targets: '.ml10',
+                        opacity: 0,
+                        duration: 1000,
+                        easing: "easeOutExpo",
+                        delay: 1000
+                    });
+                }
+                if(typeof(obj)!= "undefined" &&  obj.anim_type==11){
+                // Wrap every letter in a span
                     var textWrapper = document.querySelector('.ml11 .letters');
                     textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
@@ -359,9 +360,9 @@ $window.on( 'elementor/frontend/init', function() {
                         easing: "easeOutExpo",
                         delay: 1000
                     });
-                 }
-                 if(typeof(obj)!= "undefined" &&  obj.anim_type==12){
-                 // Wrap every letter in a span
+                }
+                if(typeof(obj)!= "undefined" &&  obj.anim_type==12){
+                // Wrap every letter in a span
                     var textWrapper = document.querySelector('.ml12');
                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -382,9 +383,9 @@ $window.on( 'elementor/frontend/init', function() {
                         duration: 1100,
                         delay: (el, i) => 100 + 30 * i
                     });
-                 }
-                 if(typeof(obj)!= "undefined" &&  obj.anim_type==13){
-                 // Wrap every letter in a span
+                }
+                if(typeof(obj)!= "undefined" &&  obj.anim_type==13){
+                // Wrap every letter in a span
                     var textWrapper = document.querySelector('.ml13');
                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -405,8 +406,8 @@ $window.on( 'elementor/frontend/init', function() {
                         duration: 1200,
                         delay: (el, i) => 100 + 30 * i
                     });
-                 }
-                 if(typeof(obj)!= "undefined" &&  obj.anim_type==14){
+                }
+                if(typeof(obj)!= "undefined" &&  obj.anim_type==14){
                     // Wrap every letter in a span
                     var textWrapper = document.querySelector('.ml14 .letters');
                     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -436,9 +437,40 @@ $window.on( 'elementor/frontend/init', function() {
                         delay: 1000
                     });
                 }
+        }
+    });
+
+    /**
+     * Moving Letters Finalized Here
+     * 
+     * Actually most of the part of this Widget has done by Numan but
+     * JS part had developed by(Me) Saiful Islam
+     * 
+     * @author Saiful Islam <codersaiful@gmail.com>
+     * @author B M Rafiul Alam <bmrafiul.alam@gmail.com>
+     * @since 1.1.0.8
+     */
+
+    // Moving_Letters Hooked Here
+    EF.hooks.addAction(
+        'frontend/element_ready/ultraaddons-moving-letters.default',
+        function ($scope) {
                 
-            }
-        });
+                EF.elementsHandler.addHandler(Moving_Letters, {
+                        $element: $scope,
+                        selectors: {
+                              container: '.ua-skill-chart',
+                        },
+                        scaleColor: "#ecf0f1",
+                });
+        }
+    );
+
+
+
+
+            
+
 });
 
 } (jQuery, window));
--- inc/widget/moving-letters.php
+++ inc/widget/moving-letters.php
@@ -290,15 +290,41 @@ class Moving_Letters extends Base{
     protected function render() {
         $settings = $this->get_settings_for_display();
         $anim_type = $settings['anim_type'];
+        // var_dump($anim_type);
         ?>
         <?php
             if( Plugin::$instance->editor->is_edit_mode()){
-                echo '<script>
-                anime({
-                    targets: ".ua-ml",
-                    loop:true
-                  });
-                </script>';
+                ?>
+                <script>
+                var textWrapper = document.querySelector('.ml1 .letters');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                    anime.timeline({loop: true})
+                    .add({
+                        targets: '.ml1 .letter',
+                        scale: [0.3,1],
+                        opacity: [0,1],
+                        translateZ: 0,
+                        easing: "easeOutExpo",
+                        duration: 600,
+                        delay: (el, i) => 70 * (i+1)
+                    }).add({
+                        targets: '.ml1 .line',
+                        scaleX: [0,1],
+                        opacity: [0.5,1],
+                        easing: "easeOutExpo",
+                        duration: 700,
+                        offset: '-=875',
+                        delay: (el, i, l) => 80 * (l - i)
+                    }).add({
+                        targets: '.ml1',
+                        opacity: 0,
+                        duration: 1000,
+                        easing: "easeOutExpo",
+                        delay: 1000
+                    });
+                </script>
+                <?php
             }
         ?>
         <?php
