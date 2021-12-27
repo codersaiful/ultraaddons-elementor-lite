acd208dbdb9b9951d4f750043bf5cfc157ad5e8f
rafiul7 <bmrafiul.alam@gmail.com>
2021-12-27 17:57:28 +0600

    pgi

--- inc/widget/product-tabs.php
+++ inc/widget/product-tabs.php
@@ -1146,23 +1146,7 @@ class Product_Tabs extends Base{
 		?>
 		
 	</div>
-	<nav class="ua-pagination ua-col-1">
-			<?php
-			$total_pages = $loop->max_num_pages;
 
-			if ($total_pages > 1){
-				$current_page = max(1, get_query_var('paged'));
-				echo paginate_links(array(
-					'base' => get_pagenum_link(1) . '%_%',
-					'format' => '/page/%#%',
-					'current' => $current_page,
-					'total' => $total_pages,
-					'prev_text'    => __('« Prev'),
-					'next_text'    => __('Next »'),
-				));
-			}
-			?>
-		</nav> 
 	<?php }
 	
 		/**
