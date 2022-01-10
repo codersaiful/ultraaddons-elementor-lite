22c9d1d021301e244cbc648fd9f1a614f707fd19
rafiul7 <bmrafiul.alam@gmail.com>
2022-01-10 09:43:47 +0600

    Chrat all Pushing

--- assets/js/frontend-chart.js
+++ assets/js/frontend-chart.js
@@ -9,13 +9,19 @@ $window.on( 'elementor/frontend/init', function() {
         EM = elementorModules;
     
      /**
-     * Chart Initialize Here
+     * All Chart widget Initialize Here
      * 
      * @author Saiful Islam <codersaiful@gmail.com>
      * @author B M Rafiul Alam <bmrafiul.alam@gmail.com>
-     * @since 1.1.0.8
+     * @since 1.1.0.9
      */
-     //Bar Chart
+
+   
+    /*****************************************
+    * Bar Chart Options and initialization js
+    *  @author B M Rafiul Alam
+    *****************************************/
+
       var UA_Chart = EM.frontend.handlers.Base.extend({
         onInit: function(){
             this.run();
@@ -87,9 +93,7 @@ $window.on( 'elementor/frontend/init', function() {
                       y: {
                         ticks: {
                             color: $settings.y_ticks_color,
-                        }
-                      },
-                      y: {
+                        },
                         grid: {
                           drawBorder: false,
                           color:$settings.y_grid_color,
@@ -118,7 +122,11 @@ $window.on( 'elementor/frontend/init', function() {
         }
     );
 
-     //Line Chart
+     
+   /*****************************************
+    * Line Chart Options and initialization js
+    *  @author B M Rafiul Alam
+    *****************************************/
 
      var Line_chart = EM.frontend.handlers.Base.extend({
         onInit: function(){
@@ -197,9 +205,7 @@ $window.on( 'elementor/frontend/init', function() {
                       y: {
                         ticks: {
                             color: $settings.y_ticks_color,
-                        }
-                      },
-                      y: {
+                        },
                         grid: {
                           drawBorder: false,
                           color:$settings.y_grid_color,
@@ -227,8 +233,407 @@ $window.on( 'elementor/frontend/init', function() {
             });
         }
     );
+
+  
+    /*****************************************
+    * Pie Chart Options and initialization js
+    *  @author B M Rafiul Alam
+    *****************************************/
+
+      var Pie_chart = EM.frontend.handlers.Base.extend({
+        onInit: function(){
+            this.run();
+        },
+        onChange: function(){
+            this.run();
+        },
+        run: function(){
+
+            var $scope = this.$element;
+           
+            /**
+             * get data on editor mode
+             */
+            var $settings  = this.getElementSettings();
+            var $id        = $scope[0].dataset.id;
+           //Get Data List
+            const $data    = $settings.list;
+
+            let dLen       = $data.length;
+            let $chartData = [],
+                $labels    = [],
+                $backgroundColor = [];
+
+            for (let i = 0; i < dLen; i++) {
+                let $d = $data[i];
+                $chartData.push($d.data);
+                $labels.push($d.labels);
+                $backgroundColor.push($d.backgroundColor);
+            }
+            //Datasets    
+            const labels =  $labels;
+
+            const pieChartData = {
+                labels: labels,
+                datasets: [{
+                  label:  $settings.legend_label,
+                  data: $chartData,
+                  backgroundColor: $backgroundColor,
+                  hoverOffset: 4
+                }]
+              };
+
+            //Config Data
+            const config = {
+                type:'pie',
+                data: pieChartData,
+                options: {
+                    responsive: true,
+                    plugins: {
+                      title: {
+                        display: true,
+                        text: $settings.chart_title
+                      },
+                      legend: {
+                            display: true,
+                            position: $settings.legend_position,
+                            align: 'middle',
+                            labels: {
+                                color: $settings.legend_color
+                            }
+                        }
+                    },
+                    scales: {
+                      x: {
+                        ticks: {
+                          color: $settings.x_ticks_color,
+                        }
+                      },
+                      y: {
+                        ticks: {
+                            color: $settings.y_ticks_color,
+                        },
+                        grid: {
+                          drawBorder: false,
+                          color:$settings.y_grid_color,
+                        },
+                      }
+
+                    }
+                },
+            };
+            //Initialize
+            var ctx = document.getElementById('uaChart-' + $id);
+            const uaChart = new Chart(ctx,
+                config
+            );
+            
+        }
+    });
+
+    EF.hooks.addAction(
+        'frontend/element_ready/ultraaddons-pie-chart.default',
+        function ($scope) {
+            EF.elementsHandler.addHandler(Pie_chart, {
+                $element: $scope,
+            });
+        }
+    );
+
+    /*****************************************
+    * Doughnut Chart Options and initialization js
+    *  @author B M Rafiul Alam
+    *****************************************/
+
+      var Doughnut_chart = EM.frontend.handlers.Base.extend({
+        onInit: function(){
+            this.run();
+        },
+        onChange: function(){
+            this.run();
+        },
+        run: function(){
+
+            var $scope = this.$element;
+           
+            /**
+             * get data on editor mode
+             */
+            var $settings  = this.getElementSettings();
+            var $id        = $scope[0].dataset.id;
+           //Get Data List
+            const $data    = $settings.list;
+
+            let dLen       = $data.length;
+            let $chartData = [],
+                $labels    = [],
+                $backgroundColor = [];
+
+            for (let i = 0; i < dLen; i++) {
+                let $d = $data[i];
+                $chartData.push($d.data);
+                $labels.push($d.labels);
+                $backgroundColor.push($d.backgroundColor);
+            }
+            //Datasets    
+            const labels =  $labels;
+
+            const doughnutChartData = {
+                labels: labels,
+                datasets: [{
+                  label:  $settings.legend_label,
+                  data: $chartData,
+                  backgroundColor: $backgroundColor,
+                  hoverOffset: 4
+                }]
+              };
+
+            //Config Data
+            const config = {
+                type:'doughnut',
+                data: doughnutChartData,
+                options: {
+                    responsive: true,
+                    plugins: {
+                      title: {
+                        display: true,
+                        text: $settings.chart_title
+                      },
+                      legend: {
+                            display: true,
+                            position: $settings.legend_position,
+                            align: 'middle',
+                            labels: {
+                                color: $settings.legend_color
+                            }
+                        }
+                    },
+                    scales: {
+                      x: {
+                        ticks: {
+                          color: $settings.x_ticks_color,
+                        }
+                      },
+                      y: {
+                        ticks: {
+                            color: $settings.y_ticks_color,
+                        },
+                        grid: {
+                          drawBorder: false,
+                          color:$settings.y_grid_color,
+                        },
+                      }
+
+                    }
+                },
+            };
+            //Initialize
+            var ctx = document.getElementById('uaChart-' + $id);
+            const uaChart = new Chart(ctx,
+                config
+            );
+            
+        }
+    });
+
+    EF.hooks.addAction(
+        'frontend/element_ready/ultraaddons-doughnut-chart.default',
+        function ($scope) {
+            EF.elementsHandler.addHandler(Doughnut_chart, {
+                $element: $scope,
+            });
+        }
+    );
+
+    /*****************************************
+    * Polar Area Chart Chart Options and initialization js
+    *  @author B M Rafiul Alam
+    *****************************************/
+
+     var Polar_chart = EM.frontend.handlers.Base.extend({
+      onInit: function(){
+          this.run();
+      },
+      onChange: function(){
+          this.run();
+      },
+      run: function(){
+
+          var $scope = this.$element;
+         
+          /**
+           * get data on editor mode
+           */
+          var $settings  = this.getElementSettings();
+          var $id        = $scope[0].dataset.id;
+         //Get Data List
+          const $data    = $settings.list;
+
+          let dLen       = $data.length;
+          let $chartData = [],
+              $labels    = [],
+              $backgroundColor = [];
+
+          for (let i = 0; i < dLen; i++) {
+              let $d = $data[i];
+              $chartData.push($d.data);
+              $labels.push($d.labels);
+              $backgroundColor.push($d.backgroundColor);
+          }
+          //Datasets    
+          const labels =  $labels;
+
+          const polarChartData = {
+              labels: labels,
+              datasets: [{
+                label:  $settings.legend_label,
+                data: $chartData,
+                backgroundColor: $backgroundColor,
+                hoverOffset: 4
+              }]
+            };
+
+          //Config Data
+          const config = {
+              type:'polarArea',
+              data: polarChartData,
+              options: {
+                  responsive: true,
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: $settings.chart_title
+                    },
+                    legend: {
+                          display: true,
+                          position: $settings.legend_position,
+                          align: 'middle',
+                          labels: {
+                              color: $settings.legend_color
+                          }
+                      }
+                  },
+                  scales: {
+                    x: {
+                      ticks: {
+                        color: $settings.x_ticks_color,
+                      }
+                    },
+                    y: {
+                      ticks: {
+                          color: $settings.y_ticks_color,
+                      },
+                      grid: {
+                        drawBorder: false,
+                        color:$settings.y_grid_color,
+                      },
+                    }
+
+                  }
+              },
+          };
+          //Initialize
+          var ctx = document.getElementById('uaChart-' + $id);
+          const uaChart = new Chart(ctx,
+              config
+          );
+          
+      }
+  });
+
+  EF.hooks.addAction(
+      'frontend/element_ready/ultraaddons-polar-chart.default',
+      function ($scope) {
+          EF.elementsHandler.addHandler(Polar_chart, {
+              $element: $scope,
+          });
+      }
+  );
+
+  /*****************************************
+    * Mixed Chart Options and initialization js
+    *  @author B M Rafiul Alam
+    *****************************************/
+
+   var Mixed_chart = EM.frontend.handlers.Base.extend({
+    onInit: function(){
+        this.run();
+    },
+    onChange: function(){
+        this.run();
+    },
+    run: function(){
+
+        var $scope = this.$element;
+       
+        /**
+         * get data on editor mode
+         */
+        var $settings  = this.getElementSettings();
+        var $id        = $scope[0].dataset.id;
+       //Get Data List
+        const $barDataList    = $settings.bar_data_list;
+        const $lineDataList    = $settings.line_data_list;
+
+        let $dLen       = $barDataList.length;
+        let $chartData = [],
+            $labels    = [],
+            $backgroundColor = [];
+
+        for (let i = 0; i < $dLen; i++) {
+            let $d = $barDataList[i];
+            $chartData.push($d.data);
+            $labels.push($d.labels);
+            $backgroundColor.push($d.backgroundColor);
+        }
+        //Datasets    
+        const labels =  $labels;
+        //Config Data
+        const config = {
+          type: "bar",
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                type: "bar",
+                backgroundColor: $backgroundColor,
+                borderColor:$settings.borderColor,
+                borderWidth: 1,
+                label:$settings.legend_label,
+                data: $chartData,
+                hoverOffset: 4
+              },
+              {
+                type: "line",
+                label:$settings.legend_label,
+                data: $chartData,
+                borderColor:$settings.borderColor,
+                lineTension: 0,
+                hoverOffset: 4
+              }
+            ]
+          }
+        };
+        //Initialize
+        var ctx = document.getElementById('uaChart-' + $id);
+        const uaChart = new Chart(ctx,
+            config
+        );
+        
+        
+    }
+});
+
+EF.hooks.addAction(
+    'frontend/element_ready/ultraaddons-mixed-chart.default',
+    function ($scope) {
+        EF.elementsHandler.addHandler(Mixed_chart, {
+            $element: $scope,
+        });
+    }
+);
   
 
-});
+}); //end elementor/frontend/init 
 
 } (jQuery, window));
