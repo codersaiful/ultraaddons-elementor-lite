9a6a4f68faa86bc77bd9aebf0b4f608a6059ed8a
Saiful Islam <codersaiful@gmail.com>
2022-07-25 16:39:39 +0600

    1.1.3 version updated and tested upto also updated

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
  * Plugin Name: Addons - UltraAddons Elementor Lite
  * Plugin URI: https://ultraaddons.com/
  * Description: Elementor Addons Plugin. Build your desired page just few click. Easy to use and useable for any theme and plugin. Available many filter.
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: CodeAstrology
  * Author URI: https://profiles.wordpress.org/codersaiful/#content-plugins
  * License: GPL3+
@@ -12,11 +12,11 @@
  * Domain Path: /languages/
  * 
  * Requires at least:    4.0.0
- * Tested up to:         5.9
+ * Tested up to:         6.0.3
  * WC requires at least: 3.0.0
  * WC tested up to: 	 6.1.1
- * Elementor tested up to: 3.5.4
- * Elementor Pro tested up to: 5.11.0
+ * Elementor tested up to: 3.9.7
+ * Elementor Pro tested up to: 7.11.0
  *
  * @package UltraAddons
  * @category Addons
@@ -46,7 +46,7 @@
 
 defined( 'ABSPATH' ) || die();
 
-define( 'ULTRA_ADDONS_VERSION', '1.1.0.12' );
+define( 'ULTRA_ADDONS_VERSION', '1.1.3.0' );
 define( 'ULTRA_ADDONS__FILE__', __FILE__ );
 define( 'ULTRA_ADDONS_BASE_NAME', plugin_basename( __FILE__ ) );
 define( 'ULTRA_ADDONS_DIR', plugin_dir_path( ULTRA_ADDONS__FILE__ ) );
