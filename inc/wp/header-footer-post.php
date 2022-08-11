4c49bdbacceb4fd9599b9d383c41165eb166fe4c
Saiful Islam <codersaiful@gmail.com>
2022-08-11 14:39:52 +0600

    hf-post

--- inc/wp/header-footer-post.php
+++ inc/wp/header-footer-post.php
@@ -221,8 +221,7 @@ class Header_Footer_Post{
 		$values = get_post_meta($post_id,'ua_display',true);
 		
 		$rules_arr = $values[$rule] ?? array();
-		// var_dump($rules_arr);
-		// return;
+		
 		$field_arr = [
 			'Basic' => [
 
